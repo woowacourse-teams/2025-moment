c6b0078b0d0aba32e5950c602b268dfc3daa201a
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-07-10 22:45:21 +0900

    chore: 초기 개발 환경 설정

---
+++ server/src/test/java/moment/MomentApplicationTests.java
@@ -0,0 +1,13 @@
+package moment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
