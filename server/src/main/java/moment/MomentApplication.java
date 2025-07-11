5383a5e3781b356bb8c9a948aaee9db06c272f8f
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-07-11 13:34:18 +0900

    chore: server 초기 세팅

---
+++ server/src/main/java/moment/MomentApplication.java
@@ -0,0 +1,13 @@
+package moment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomentApplication.class, args);
+	}
+
+}
