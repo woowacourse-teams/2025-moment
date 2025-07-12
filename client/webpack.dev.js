651a8859ce1a9379e20df7e4849bf14d63aff2f9
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-12 20:12:44 +0900

    chore: webpack 설정 파일이 es module 방식일 때 Node.js에서 __dirname을 알 수 있도록 설정

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -1,6 +1,10 @@
 import { merge } from 'webpack-merge';
 import common from './webpack.common.js';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default merge(common, {
   mode: 'development',
@@ -9,6 +13,20 @@ export default merge(common, {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+          },
+        },
+      },
+    ],
   },
   devServer: {
     static: {
