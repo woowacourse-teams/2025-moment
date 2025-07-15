144f3521c7781e76a801130727f3e7d7d7db6e93
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-15 15:28:36 +0900

    chore: storybook 컴파일러 설정 및 초기설정

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -1,11 +1,12 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-docs'],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
   },
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-webpack5-compiler-swc'],
 };
+
 export default config;
