f887f6edcce002ddab1f9fcbedb7adfb97137969
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-15 20:09:39 +0900

    styles: Emotion css초기화를 위해 GlobalStyles 구현

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
 import { router } from '@/routes';
+import GlobalStyles from '@/styles/GlobalStyles';
 import { RouterProvider } from 'react-router';
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </>
+  );
 };
 
 export default App;
