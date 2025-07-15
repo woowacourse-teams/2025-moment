e15884f41b35b58f04919a7f1fe51eefdb5dfdf6
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-15 22:08:51 +0900

    feat: LoginButton 컴포넌트 구현

---
+++ client/src/components/auth/LoginButton.tsx
@@ -0,0 +1,22 @@
+import { Button } from '@/components/common/Button';
+import { colors } from '@/constants/colors';
+import { css } from '@emotion/react';
+
+const loginStyle = css`
+  background-color: transparent;
+  color: #fff;
+  border: 1px solid ${colors.border.primary};
+  border-radius: 50px;
+  padding: 10px 20px;
+  font-size: 16px;
+  font-weight: 600;
+
+  &:hover {
+    transform: scale(1.05);
+    transition: transform 0.3s ease;
+  }
+`;
+
+export const LoginButton = () => {
+  return <Button title="로그인" onClick={() => {}} css={loginStyle} />;
+};
