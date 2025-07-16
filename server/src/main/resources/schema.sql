7130ba9334873287b0a5bb58fc04c89136775954
mimi <110226866+hongkikii@users.noreply.github.com>
2025-07-16 15:00:36 +0900

    chore: timestamp default 설정 제거

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users
 	email VARCHAR(255) NOT NULL UNIQUE,
 	password VARCHAR(255) NOT NULL,
 	nickname VARCHAR(255) NOT NULL UNIQUE,
-	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+	created_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS moments
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS moments
 	momenter_id BIGINT NOT NULL,
 	content VARCHAR(255) NOT NULL,
 	is_matched BOOLEAN NOT NULL,
-	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	created_at TIMESTAMP NOT NULL,
     CONSTRAINT fk_moments_users
         FOREIGN KEY (momenter_id)
         REFERENCES users (id)
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS matchings
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     moment_id BIGINT NOT NULL,
     commenter_id BIGINT NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP NOT NULL,
     CONSTRAINT fk_matchings_moments
         FOREIGN KEY (moment_id)
             REFERENCES moments (id),
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS comments
     commenter_id BIGINT NOT NULL,
     moment_id BIGINT NOT NULL,
     content VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP NOT NULL,
     CONSTRAINT fk_comments_moments
         FOREIGN KEY (moment_id)
             REFERENCES moments (id),
@@ -53,7 +53,7 @@ CREATE TABLE IF NOT EXISTS emojis
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id BIGINT NOT NULL,
     comment_id BIGINT NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP NOT NULL,
     type VARCHAR(255) NOT NULL,
     CONSTRAINT fk_emojis_users
         FOREIGN KEY (user_id)
