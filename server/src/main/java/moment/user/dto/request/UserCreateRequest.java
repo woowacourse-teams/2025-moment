1d5487260a3a51347f6ba5bc803a66e3e5296ad9
mimi <110226866+hongkikii@users.noreply.github.com>
2025-07-17 14:38:14 +0900

    feat: 회원가입 Swagger docs 추가

--- server/src/main/java/moment/user/dto/request/UserCreateRequest.java
+++ server/src/main/java/moment/user/dto/request/UserCreateRequest.java
@@ -1,8 +1,22 @@
 package moment.user.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import moment.user.domain.User;
 
-public record UserCreateRequest(String email, String password, String rePassword, String nickname) {
+@Schema(description = "회원가입 요청 DTO")
+public record UserCreateRequest(
+        @Schema(description = "사용자 이메일(아이디)", example = "mimi@icloud.com")
+        String email,
+
+        @Schema(description = "사용자 비밀번호", example = "1234")
+        String password,
+
+        @Schema(description = "비밀번호 확인", example = "1234")
+        String rePassword,
+
+        @Schema(description = "사용자 닉네임", example = "mimi")
+        String nickname
+) {
 
     public User toUser() {
         return new User(email, password, nickname);
--- server/src/main/java/moment/user/presentation/UserController.java
+++ server/src/main/java/moment/user/presentation/UserController.java
@@ -1,6 +1,13 @@
 package moment.user.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import moment.global.dto.response.ErrorResponse;
 import moment.global.dto.response.SuccessResponse;
 import moment.user.application.UserService;
 import moment.user.dto.request.UserCreateRequest;
@@ -11,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "User API", description = "사용자 관련 API 명세")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
@@ -18,6 +26,27 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "회원 가입", description = "새로운 사용자를 등록합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "회원가입 성공"),
+            @ApiResponse(responseCode = "400", description = """
+                    - [U-004] 유효하지 않은 이메일 형식입니다.
+                    - [U-005] 유효하지 않은 비밀번호 형식입니다.
+                    - [U-006] 유효하지 않은 닉네임 형식입니다.
+                    - [U-007] 비밀번호가 일치하지 않습니다.
+                    """,
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+            ),
+            @ApiResponse(responseCode = "404", description = """
+                    - [U-002] 존재하지 않는 사용자입니다.
+                    """,
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "409", description = """
+                    - [U-001] 이미 가입된 사용자입니다.
+                    - [U-003] 이미 존재하는 닉네임입니다.
+                    """,
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+    })
     @PostMapping("/signup")
     public ResponseEntity<SuccessResponse<Void>> createUser(@RequestBody UserCreateRequest request) {
         userService.addUser(request);
