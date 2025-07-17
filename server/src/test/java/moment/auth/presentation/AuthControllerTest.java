294cdc6991b2bb2bb56ce64db25d10ed6ea0d86c
Firedrago95 <yonghwa95@naver.com>
2025-07-17 17:45:49 +0900

    test: 로그인 통합 테스트 구현

--- server/src/test/java/moment/auth/presentation/AuthControllerTest.java
+++ server/src/test/java/moment/auth/presentation/AuthControllerTest.java
@@ -1,9 +1,13 @@
 package moment.auth.presentation;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import io.jsonwebtoken.Claims;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import moment.auth.application.AuthService;
 import moment.auth.dto.LoginRequest;
+import moment.auth.infrastructure.JwtTokenManager;
 import moment.user.domain.User;
 import moment.user.infrastructure.UserRepository;
 import org.junit.jupiter.api.Test;
@@ -21,7 +25,7 @@ import org.springframework.test.context.ActiveProfiles;
 class AuthControllerTest {
 
     @Autowired
-    AuthService authService;
+    JwtTokenManager jwtTokenManager;
 
     @Autowired
     UserRepository userRepository;
@@ -29,7 +33,7 @@ class AuthControllerTest {
     @Test
     void 로그인에_성공한다() {
         // given
-        userRepository.save(new User("ekorea623@gmail.com", "1q2w3e4r", "drago"));
+        User user = userRepository.save(new User("ekorea623@gmail.com", "1q2w3e4r", "drago"));
         LoginRequest request = new LoginRequest("ekorea623@gmail.com", "1q2w3e4r");
 
         // when
@@ -41,10 +45,12 @@ class AuthControllerTest {
                 .statusCode(HttpStatus.OK.value())
                 .extract().cookie("token");
 
-
-
         // then
+        Claims claims = jwtTokenManager.extractClaims(token);
 
+        assertAll(
+                () -> assertThat(claims.getSubject()).isEqualTo(String.valueOf(user.getId())),
+                () -> assertThat(claims.get("email", String.class)).isEqualTo(user.getEmail())
+        );
     }
-
 }
