d345c9d72ce58d6e0f23c16dd6cc16f2ac725e95
haku <hakukudh@gmail.com>
2025-07-17 15:51:00 +0900

    style: card padding 변경
    Co-authored-by: sanghee01 <sang6299@naver.com>

--- client/src/shared/ui/card/Card.styles.ts
+++ client/src/shared/ui/card/Card.styles.ts
@@ -1,23 +1,21 @@
-import { CustomTheme } from "@/app/styles/theme";
-import styled from "@emotion/styled";
+import { CustomTheme } from '@/app/styles/theme';
+import styled from '@emotion/styled';
 
-export type cardWidth = "small" | "medium" | "large";
+export type cardWidth = 'small' | 'medium' | 'large';
 
-const CardStyles ={
-
-    card: (theme: CustomTheme, width: cardWidth) => `
+const CardStyles = {
+  card: (theme: CustomTheme, width: cardWidth) => `
     display: flex;
     flex-direction: column;
-    gap: 5px;
+    gap: 15px;
     width: ${theme.typography.cardWidth[width]};
-    padding: 10px 20px;
+    padding: 20px 30px;
     background-color: color-mix(in srgb, ${theme.colors.slate800} 60%, transparent);
     border-radius: 10px;
     border: 1px solid ${theme.colors.gray700};
-    
-    `
-}
+    `,
+};
 
-export const Card = styled.div<{width: cardWidth}>`
-    ${({theme, width}) => CardStyles.card(theme, width)}
+export const Card = styled.div<{ width: cardWidth }>`
+  ${({ theme, width }) => CardStyles.card(theme, width)}
 `;
