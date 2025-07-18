a88861502196cd8c9d8661a6c6f746ed1c35d8f6
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-18 18:47:23 +0900

    build: babel, wepback, jest 불필요한 초기세팅 제거

--- client/test/sum.test.tsx
+++ client/test/sum.test.tsx
@@ -1,10 +1,16 @@
+import { theme } from '@/app/styles/theme';
 import { Button } from '@/shared/ui/Button';
+import { ThemeProvider } from '@emotion/react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 test('버튼 클릭 이벤트 동작', async () => {
   const onClick = jest.fn();
-  render(<Button title="Click me" onClick={onClick} variant="primary" />);
+  render(
+    <ThemeProvider theme={theme}>
+      <Button title="Click me" onClick={onClick} variant="primary" />
+    </ThemeProvider>,
+  );
 
   await userEvent.click(screen.getByText('Click me'));
 
