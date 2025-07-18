630c604e6284ae6dd7dd2b4b82483d1ec1c4991b
이상희 <sang6299@naver.com>
2025-07-18 19:32:11 +0900

    refactor: 외부에서 사용하지 않는 파일 export 제거

--- client/src/shared/ui/index.ts
+++ client/src/shared/ui/index.ts
@@ -2,4 +2,3 @@ export { Logo } from './logo/Logo';
 export { Card } from './card/Card';
 export { TextArea } from './textArea/TextArea';
 export { Button } from './button/Button';
-export { TitleContainer } from './titleContainer/TitleContainer';
