2b2ba62c8b5f45dc4834653175b6f4c23a9b516a
이상희 <sang6299@naver.com>
2025-07-18 10:45:40 +0900

    refactor: TitleContainer props를 args로 받아오도록 수정

--- client/src/shared/ui/titleContainer/TitleContainer.stories.tsx
+++ client/src/shared/ui/titleContainer/TitleContainer.stories.tsx
@@ -5,8 +5,8 @@ const meta: Meta = {
   title: 'Example/TitleContainer',
   component: TitleContainer,
   args: {
-    title: 'Title',
-    subtitle: 'Subtitle',
+    title: '오늘의 모멘트',
+    subtitle: '하루에 한 번, 당신의 특별한 모멘트를 공유해보세요',
   },
 };
 
@@ -16,11 +16,6 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   render: args => {
-    return (
-      <TitleContainer
-        title="오늘의 모멘트"
-        subtitle="하루에 한 번, 당신의 특별한 모멘트를 공유해보세요"
-      />
-    );
+    return <TitleContainer title={args.title} subtitle={args.subtitle} />;
   },
 };
