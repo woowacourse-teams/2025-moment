d878673fff2bbd69d3b46fa40e1ca8c1b3c40515
yush <yush2202@gmail.com>
2025-07-18 14:00:34 +0900

    refactor: 불필요한 어노테이션 삭제

--- server/src/main/java/moment/user/application/UserQueryService.java
+++ server/src/main/java/moment/user/application/UserQueryService.java
@@ -1,9 +1,7 @@
 package moment.user.application;
 
 import moment.user.domain.User;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface UserQueryService {
 
     User getUserById(Long id);
