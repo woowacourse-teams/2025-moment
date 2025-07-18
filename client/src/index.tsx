bea7dde938a2a327c44a1aa99d700bf9f24cb232
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-18 17:31:04 +0900

    feat: index.ts에 development env일때만 실행되도록 설정

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,28 @@
 import App from '@/app/App';
 import { createRoot } from 'react-dom/client';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement!);
-root.render(<App />);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+
+  return worker.start({
+    onUnhandledRequest: 'warn',
+  });
+}
+
+async function startApp() {
+  await enableMocking();
+
+  const rootElement = document.getElementById('root');
+  if (!rootElement) {
+    throw new Error('Root element not found');
+  }
+
+  const root = createRoot(rootElement);
+  root.render(<App />);
+}
+
+startApp().catch(console.error);
