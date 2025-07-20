6dce26b6c7385f83a92f880c4d7ba0b665c1edd5
haku <hakukudh@gmail.com>
2025-07-20 22:58:09 +0900

    feat: TodaySuccessContent 컴포넌트 구현

---
+++ client/src/widgets/today/TodaySuccessContent.tsx
@@ -0,0 +1,27 @@
+import { RequestButton } from '@/features/auth/ui/requestButton';
+import { Card, TextArea } from '@/shared/ui';
+import { CardSuccessContainer } from '@/shared/ui/card/CardSuccessContainer';
+import { CheckCircle, MessageSquare } from 'lucide-react';
+
+interface TodaySuccessContentProps {
+  onBack: () => void;
+}
+
+export const TodaySuccessContent = ({ onBack }: TodaySuccessContentProps) => {
+  return (
+    <>
+      <Card.Content>
+        <CardSuccessContainer
+          Icon={CheckCircle}
+          title="오늘의 모멘트를 공유했어요!"
+          subtitle={
+            '당신의 모멘트가 누군가에게 전달되었습니다.\n내일 또 다른 모멘트를 공유해보세요'
+          }
+        ></CardSuccessContainer>
+      </Card.Content>
+      <Card.Action position="center">
+        <RequestButton Icon={MessageSquare} title="받은 모멘트 보기" onClick={onBack} />
+      </Card.Action>
+    </>
+  );
+};
