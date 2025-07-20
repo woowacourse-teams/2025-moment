0863f93c7c59a13e2bc03c3b28b9e6c3769c12fd
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-21 02:50:34 +0900

    feat: LoginForm 컴포넌트 구현

---
+++ client/src/features/auth/ui/LoginForm.tsx
@@ -0,0 +1,36 @@
+import * as S from './LoginForm.styles';
+
+export const LoginForm = () => {
+  return (
+    <S.LoginFormWrapper>
+      <S.LoginFormTitleContainer>
+        <S.LoginLogoTitleContainer>
+          <S.LogoImage src="/logo.webp" alt="Moment Logo Image" />
+          <S.LogoTitle>모멘트</S.LogoTitle>
+        </S.LoginLogoTitleContainer>
+        <S.LoginTitle>로그인</S.LoginTitle>
+        <S.LoginDescription>이메일과 비밀번호를 입력하여 로그인해주세요.</S.LoginDescription>
+      </S.LoginFormTitleContainer>
+      <S.LoginFormContent>
+        <S.InputGroup>
+          <S.Label htmlFor="email">이메일</S.Label>
+          <S.Input id="email" type="email" placeholder="이메일을 입력해주세요" />
+        </S.InputGroup>
+        <S.InputGroup>
+          <S.Label htmlFor="password">비밀번호</S.Label>
+          <S.Input id="password" type="password" placeholder="비밀번호를 입력해주세요" />
+        </S.InputGroup>
+      </S.LoginFormContent>
+      <S.LoginFooter>
+        <S.LoginButton>로그인</S.LoginButton>
+        <S.LoginFooterContent>
+          <S.LoginForgotPassword>비밀번호를 잊으셨나요?</S.LoginForgotPassword>
+          <S.LoginSignupContainer>
+            <span>아직 회원이 아니신가요?</span>
+            <S.LoginSignupLink>회원가입</S.LoginSignupLink>
+          </S.LoginSignupContainer>
+        </S.LoginFooterContent>
+      </S.LoginFooter>
+    </S.LoginFormWrapper>
+  );
+};
