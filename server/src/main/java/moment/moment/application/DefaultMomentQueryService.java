d1d4f40fe9b41e32170ab76a9767d9c3ca56415a
yush <yush2202@gmail.com>
2025-07-20 16:20:23 +0900

    feat: Moment 조회 전용 서비스 기능 구현 및 테스트

---
+++ server/src/main/java/moment/moment/application/DefaultMomentQueryService.java
@@ -0,0 +1,23 @@
+package moment.moment.application;
+
+import lombok.RequiredArgsConstructor;
+import moment.global.exception.ErrorCode;
+import moment.global.exception.MomentException;
+import moment.moment.domain.Moment;
+import moment.moment.infrastructure.MomentRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class DefaultMomentQueryService implements MomentQueryService {
+
+    private final MomentRepository momentRepository;
+
+    @Override
+    public Moment getMomentById(Long id) {
+        return momentRepository.findById(id)
+                .orElseThrow(() -> new MomentException(ErrorCode.MOMENT_NOT_FOUND));
+    }
+}
