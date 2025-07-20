ad34ff62dc16be3b6dd2c5a9204ae90c1c48a5bf
haku <hakukudh@gmail.com>
2025-07-20 22:58:36 +0900

    feat: TodayForm 사용자 입력부 조건부 렌더링 추가

--- client/src/features/auth/ui/TodayForm.tsx
+++ client/src/features/auth/ui/TodayForm.tsx
@@ -1,8 +1,20 @@
-import { Card, TextArea } from '@/shared/ui';
-import { Send, Star } from 'lucide-react';
-import { RequestButton } from './requestButton';
+import { Card } from '@/shared/ui';
+import { Star } from 'lucide-react';
+import { useCallback, useState } from 'react';
+import { TodaySuccessContent } from '@/widgets/today/TodaySuccessContent';
+import { TodayWriteContent } from '@/widgets/today/TodayWriteContent';
 
 export function TodayForm() {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = useCallback(() => {
+    setIsSubmitted(true);
+  }, []);
+
+  const handleBack = useCallback(() => {
+    setIsSubmitted(false);
+  }, []);
+
   return (
     <Card width="medium">
       <Card.TitleContainer
@@ -10,16 +22,11 @@ export function TodayForm() {
         title="모멘트 공유하기"
         subtitle="뿌듯한 순간, 위로받고 싶은 순간, 기쁜 순간 모든 모멘트를 자유롭게 적어보세요"
       ></Card.TitleContainer>
-      <Card.Content>
-        <TextArea
-          placeholder="오늘 어떤 모멘트를 경험하셨나요? 솔직한 마음을 들려주세요..."
-          height="medium"
-        />
-      </Card.Content>
-      <Card.Action position="space-between">
-        <p>0 / 100</p>
-        <RequestButton />
-      </Card.Action>
+      {!isSubmitted ? (
+        <TodayWriteContent onSubmit={handleSubmit} />
+      ) : (
+        <TodaySuccessContent onBack={handleBack} />
+      )}
     </Card>
   );
 }
