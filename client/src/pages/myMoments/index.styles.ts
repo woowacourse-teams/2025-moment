3cc50452c30e0a01158466f358e5f97c3c01f7ca
이상희 <sang6299@naver.com>
2025-07-21 17:25:28 +0900

    feat: 나의 모멘트 페이지 구현

---
+++ client/src/pages/myMoments/index.styles.ts
@@ -0,0 +1,41 @@
+import styled from '@emotion/styled';
+
+export const MyMomentsPageContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  margin: 20px 0;
+`;
+
+export const TitleContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const TitleWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 8px;
+`;
+
+export const TimeStamp = styled.span`
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors['gray-400']};
+`;
+
+export const Title = styled.span`
+  font-size: ${({ theme }) => theme.typography.title.fontSize.small};
+  font-weight: ${({ theme }) => theme.typography.fontWeight.large};
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export const MomentsContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
