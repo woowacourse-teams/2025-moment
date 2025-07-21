84e2875df1a1286128affaa60a76f9b5b56721f9
Firedrago95 <yonghwa95@naver.com>
2025-07-21 15:58:55 +0900

    feat: `@Transactional` 에너테이션 추가

--- server/src/main/java/moment/reply/application/DefaultEmojiQueryService.java
+++ server/src/main/java/moment/reply/application/DefaultEmojiQueryService.java
@@ -6,9 +6,11 @@ import moment.comment.domain.Comment;
 import moment.reply.domain.Emoji;
 import moment.reply.infrastructure.EmojiRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class DefaultEmojiQueryService implements EmojiQueryService {
 
     private final EmojiRepository emojiRepository;
