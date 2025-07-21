7b921bca3b3aaccc1a0689cfe75786d215b7d650
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-22 03:51:25 +0900

    feat: queryClient 컴포넌트 구현 및 Provider로 App 감싸는 로직 추가

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,6 +1,8 @@
+import { queryClient } from '@/app/lib/queryClient';
 import { router } from '@/app/routes';
-import { UserProvider } from '@/features/auth/context/UserProvider';
 import { ThemeProvider } from '@emotion/react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router';
 import GlobalStyles from './styles/GlobalStyles';
 import { theme } from './styles/theme';
@@ -8,12 +10,13 @@ import { theme } from './styles/theme';
 const App = () => {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <UserProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
           <RouterProvider router={router} />
-        </UserProvider>
-      </ThemeProvider>
+          {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   );
 };
