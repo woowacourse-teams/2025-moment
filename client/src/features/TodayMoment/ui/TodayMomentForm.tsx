c00d4ba0ee786ec482c6ddfebdc06950e3023fe0
haku <hakukudh@gmail.com>
2025-07-21 15:55:11 +0900

    style: 모멘트 요청 성공 UI 수정

--- client/src/features/TodayMoment/ui/TodayMomentForm.tsx
+++ client/src/features/TodayMoment/ui/TodayMomentForm.tsx
@@ -1,5 +1,4 @@
 import { Card } from '@/shared/ui';
-import { Star } from 'lucide-react';
 import { TodayMomentSuccessContent } from '@/widgets/todayMoment/TodayMomentSuccessContent';
 import { TodayMomentWriteContent } from '@/widgets/todayMoment/TodayMomentWriteContent';
 import { useSubmitted } from '@/features/TodayMoment/hook/useSubmitted';
@@ -9,11 +8,6 @@ export function TodayMomentForm() {
 
   return (
     <Card width="medium">
-      <Card.TitleContainer
-        Icon={Star}
-        title="모멘트 공유하기"
-        subtitle="뿌듯한 순간, 위로받고 싶은 순간, 기쁜 순간 모든 모멘트를 자유롭게 적어보세요"
-      />
       {!isSubmitted ? (
         <TodayMomentWriteContent onSubmit={handleSubmit} />
       ) : (
--- client/src/widgets/todayMoment/TodayMomentWriteContent.tsx
+++ client/src/widgets/todayMoment/TodayMomentWriteContent.tsx
@@ -1,6 +1,6 @@
 import { RequestButton } from '@/features/TodayMoment/ui/RequestButton';
 import { Card, TextArea } from '@/shared/ui';
-import { Send } from 'lucide-react';
+import { Send, Star } from 'lucide-react';
 import { useState } from 'react';
 
 interface TodayMomentWriteContentProps {
@@ -23,6 +23,11 @@ export const TodayMomentWriteContent = ({ onSubmit }: TodayMomentWriteContentPro
   };
   return (
     <>
+      <Card.TitleContainer
+        Icon={Star}
+        title="모멘트 공유하기"
+        subtitle="뿌듯한 순간, 위로받고 싶은 순간, 기쁜 순간 모든 모멘트를 자유롭게 적어보세요"
+      />
       <Card.Content>
         <TextArea
           placeholder="오늘 어떤 모멘트를 경험하셨나요? 솔직한 마음을 들려주세요..."
