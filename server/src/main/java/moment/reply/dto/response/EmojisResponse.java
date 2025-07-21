e797d880fc46c8a852fdac1fdec30f1d04b3591c
Firedrago95 <yonghwa95@naver.com>
2025-07-21 15:05:32 +0900

    feat: 이모지 조회 반환객체에 id 추가

--- server/src/main/java/moment/reply/dto/response/EmojisResponse.java
+++ server/src/main/java/moment/reply/dto/response/EmojisResponse.java
@@ -1,15 +1,24 @@
 package moment.reply.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import moment.reply.domain.Emoji;
 import moment.reply.domain.EmojiType;
 
+@Schema(description = "Emoji 조회 응답")
 public record EmojisResponse(
+        @Schema(description = "조회된 Emoji id")
+        Long id,
+
+        @Schema(description = "조회된 Emoji 타입")
         EmojiType emojiType,
+
+        @Schema(description = "조회된 Emoji 작성자")
         String userName
 ) {
 
     public static EmojisResponse from(Emoji emoji) {
         return new EmojisResponse(
+                emoji.getId(),
                 emoji.getEmojiType(),
                 emoji.getComment().getCommenter().getNickname()
         );
