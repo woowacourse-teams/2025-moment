de3d8132c6bc7b986642f63e8d607e68184eab70
Firedrago95 <yonghwa95@naver.com>
2025-07-22 10:50:30 +0900

    fix: CORS 설정 변경

--- server/src/main/java/moment/global/config/WebConfig.java
+++ server/src/main/java/moment/global/config/WebConfig.java
@@ -22,8 +22,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("*")
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "PATCH","DELETE");
     }
