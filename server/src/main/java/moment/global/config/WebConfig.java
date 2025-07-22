d49b966f7960c0ee6c472499884b7bb42cc0769f
Firedrago95 <yonghwa95@naver.com>
2025-07-22 10:31:50 +0900

    feat: CORS 설정

--- server/src/main/java/moment/global/config/WebConfig.java
+++ server/src/main/java/moment/global/config/WebConfig.java
@@ -6,6 +6,7 @@ import moment.auth.application.AuthService;
 import moment.auth.presentation.LoginUserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -17,5 +18,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginUserArgumentResolver(authService));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "PATCH","DELETE");
     }
 }
