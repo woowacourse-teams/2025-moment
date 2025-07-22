11405d3c081f602c6e56b6346807380898751c47
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-22 11:07:15 +0900

    styles: 현재 href위치에 따른 navitem 색상 변경 적용 및 공통 스타일 구현

--- client/src/app/layout/ui/Navbar.styles.ts
+++ client/src/app/layout/ui/Navbar.styles.ts
@@ -1,5 +1,9 @@
 import styled from '@emotion/styled';
 
+interface NavItemProps {
+  $isActive?: boolean;
+}
+
 export const Navbar = styled.nav`
   height: 80px;
   background-color: transparent;
@@ -15,4 +19,25 @@ export const Navbar = styled.nav`
   z-index: 100;
   backdrop-filter: blur(10px);
   border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+`;
+
+export const NavItems = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 24px;
+`;
+
+export const NavItem = styled.div<NavItemProps>`
+  display: flex;
+  align-items: center;
+  gap: 24px;
+  color: ${({ theme, $isActive }) => ($isActive ? theme.colors.white : theme.colors['gray-400'])};
+  font-size: 1.2rem;
+  font-weight: 600;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.white};
+  }
+
+  transition: color 0.3s ease;
 `;
