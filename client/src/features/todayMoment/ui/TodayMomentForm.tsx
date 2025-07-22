3dfe6adbef908184560a591eb6f600d7ea8d0529
이상희 <sang6299@naver.com>
2025-07-22 15:15:21 +0900

    fix: 파일 충돌 해결

---
+++ client/src/features/todayMoment/ui/TodayMomentForm.tsx
@@ -0,0 +1,18 @@
+import { Card } from '@/shared/ui';
+import { TodayMomentSuccessContent } from '@/features/todayMoment/ui/TodayMomentSuccessContent';
+import { TodayMomentWriteContent } from '@/features/todayMoment/ui/TodayMomentWriteContent';
+import { useSubmitted } from '@/features/todayMoment/hook/useSubmitted';
+
+export function TodayMomentForm() {
+  const { isSubmitted, handleSubmit, handleBack } = useSubmitted();
+
+  return (
+    <Card width="medium">
+      {!isSubmitted ? (
+        <TodayMomentWriteContent onSubmit={handleSubmit} />
+      ) : (
+        <TodayMomentSuccessContent onBack={handleBack} />
+      )}
+    </Card>
+  );
+}
