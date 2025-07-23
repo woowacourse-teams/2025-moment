74b972a79fb68a87b7e4428d3e654a7b5b3fb70a
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-23 09:12:37 +0900

    feat: Mobile Navbar 컴포넌트 구현

--- client/src/app/layout/ui/Navbar.tsx
+++ client/src/app/layout/ui/Navbar.tsx
@@ -1,24 +1,57 @@
 import { navItems } from '@/app/layout/types/navItems';
 import { LoginButton } from '@/features/auth/ui/LoginButton';
 import { Logo } from '@/shared/ui/logo/Logo';
+import { useState } from 'react';
 import { Link, useLocation } from 'react-router';
 import * as S from './Navbar.styles';
 
 export const Navbar = () => {
   const location = useLocation();
   const currentPath = location.pathname;
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
 
   return (
     <S.Navbar>
       <Logo />
-      <S.NavItems>
+
+      <S.DesktopNavItems>
         {navItems.map(item => (
           <S.NavItem key={item.href} $isActive={currentPath === item.href}>
             <Link to={item.href}>{item.label}</Link>
           </S.NavItem>
         ))}
-      </S.NavItems>
-      <LoginButton />
+      </S.DesktopNavItems>
+
+      <S.DesktopLoginButton>
+        <LoginButton />
+      </S.DesktopLoginButton>
+
+      <S.HamburgerButton onClick={toggleMobileMenu} $isOpen={isMobileMenuOpen}>
+        {isMobileMenuOpen ? '✕' : '☰'}
+      </S.HamburgerButton>
+
+      <S.MobileMenu $isOpen={isMobileMenuOpen}>
+        <S.MobileMenuContent>
+          <S.MobileNavItems>
+            {navItems.map(item => (
+              <S.MobileNavItem key={item.href} $isActive={currentPath === item.href}>
+                <Link to={item.href} onClick={closeMobileMenu}>
+                  {item.label}
+                </Link>
+              </S.MobileNavItem>
+            ))}
+            <LoginButton />
+          </S.MobileNavItems>
+        </S.MobileMenuContent>
+      </S.MobileMenu>
     </S.Navbar>
   );
 };
