1d33b36a27e92ed935b56f71fe9b07cdf53c46d4
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-23 20:14:19 +0900

    chore: 불 필요한 코드 제거

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,18 +1,8 @@
 import { Hero } from '@/widgets/hero';
 import * as S from './index.styles';
 import { Button } from '@/shared/ui/button/Button';
-import { useEffect } from 'react';
-import { api } from '@/app/lib/api';
 
 export default function HomePage() {
-  // 프로필 조회 테스트
-  useEffect(() => {
-    const getProfile = async () => {
-      const response = await api.get('/users/me');
-      console.log(response);
-    };
-    getProfile();
-  }, []);
 
   return (
     <S.HomePageWrapper>
