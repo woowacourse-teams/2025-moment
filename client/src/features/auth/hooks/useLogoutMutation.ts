e0972e4c56528d0450f640a19c63b3797635f2b5
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-24 17:41:53 +0900

    refactor: useLogoutMutation 훅에서 로그아웃되면 profile key에 해당되는 캐싱 무효화

--- client/src/features/auth/hooks/useLogoutMutation.ts
+++ client/src/features/auth/hooks/useLogoutMutation.ts
@@ -1,10 +1,13 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { logoutUser } from '../api/logoutUser';
 
 export const useLogoutMutation = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: logoutUser,
     onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['profile'] });
       alert('로그아웃 되었습니다.');
     },
     onError: () => {
