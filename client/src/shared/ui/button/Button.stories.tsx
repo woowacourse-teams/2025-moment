f671342b96e0a64b71d84b990b9eb9d369968a7e
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-24 15:20:08 +0900

    test: Button Storybook 리팩토링

--- client/src/shared/ui/button/Button.stories.tsx
+++ client/src/shared/ui/button/Button.stories.tsx
@@ -1,18 +1,23 @@
 import { Meta, StoryObj } from '@storybook/react-webpack5';
+import { User } from 'lucide-react';
 import { Button } from './Button';
 
-const meta: Meta = {
-  title: 'Example/Button',
+const meta: Meta<typeof Button> = {
+  title: 'Shared/Button',
   component: Button,
   argTypes: {
     variant: {
       control: { type: 'radio' },
       options: ['primary', 'secondary', 'tertiary'],
     },
+    disabled: {
+      control: { type: 'boolean' },
+    },
   },
   args: {
     variant: 'primary',
     title: 'Click me',
+    disabled: false,
   },
 };
 
@@ -21,3 +26,17 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+export const WithIcon: Story = {
+  args: {
+    Icon: User,
+    title: '사용자',
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+    title: '비활성화',
+  },
+};
