02ebc87d49419edfe58e6025bd28a5dfe1fd1674
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-24 13:44:23 +0900

    feat: 사용자 프로필 들고오는 GET API함수 구현

--- client/src/features/auth/api/getProfile.ts
+++ client/src/features/auth/api/getProfile.ts
@@ -1,7 +1,7 @@
 import { api } from '@/app/lib/api';
-import { ProfileResponse } from '../types/profile';
+import { Profile, ProfileResponse } from '../types/profile';
 
-export const getProfile = async (): Promise<ProfileResponse> => {
+export const getProfile = async (): Promise<Profile> => {
   const response = await api.get<ProfileResponse>('/users/me');
-  return response.data;
+  return response.data.data;
 };
