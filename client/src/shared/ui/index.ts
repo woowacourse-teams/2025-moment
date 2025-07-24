ede1e3a7d29d0e6acdefaaeb1059c619196d204e
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-24 19:30:14 +0900

    feat: skeleton endpoint 추가

--- client/src/shared/ui/index.ts
+++ client/src/shared/ui/index.ts
@@ -2,4 +2,5 @@ export { Button } from './button/Button';
 export { Card } from './card/Card';
 export { Logo } from './logo/Logo';
 export { SimpleCard } from './simpleCard/SimpleCard';
+export { CommonSkeletonCard } from './skeleton';
 export { TextArea } from './textArea/TextArea';
