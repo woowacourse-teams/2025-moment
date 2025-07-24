d71c3906f9d2fd0b26df6830c25230f070d43ed6
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-07-25 08:57:36 +0900

    fix: 모멘트에 이미 코멘트 존재하는 경우 코멘트 등록 시 예외 발생하도록 수정

--- server/src/test/java/moment/comment/infrastructure/CommentRepositoryTest.java
+++ server/src/test/java/moment/comment/infrastructure/CommentRepositoryTest.java
@@ -1,13 +1,8 @@
 package moment.comment.infrastructure;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
-
-import java.util.List;
 import moment.comment.domain.Comment;
 import moment.moment.domain.Moment;
 import moment.moment.infrastructure.MomentRepository;
-import moment.reply.infrastructure.EmojiRepository;
 import moment.user.domain.User;
 import moment.user.infrastructure.UserRepository;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -16,15 +11,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 @DataJpaTest
 @DisplayNameGeneration(ReplaceUnderscores.class)
 class CommentRepositoryTest {
 
     @Autowired
     private CommentRepository commentRepository;
-
-    @Autowired
-    private EmojiRepository emojiRepository;
 
     @Autowired
     private MomentRepository momentRepository;
@@ -56,5 +53,37 @@ class CommentRepositoryTest {
                 () -> assertThat(comments.getFirst()).isEqualTo(savedComment),
                 () -> assertThat(comments.getFirst().getMoment()).isEqualTo(savedMoment)
         );
+    }
+
+    @Test
+    void Momment의_Comment가_존재하면_true를_반환한다() {
+        // given
+        User momenter = new User("kiki@icloud.com", "1234", "kiki");
+        userRepository.save(momenter);
+
+        User commenter = new User("hippo@gmail.com", "1234", "hippo");
+        userRepository.save(commenter);
+
+        Moment moment = new Moment("오늘 하루는 힘든 하루~", true, momenter);
+        momentRepository.save(moment);
+
+        Comment comment = new Comment("첫 번째 댓글", commenter, moment);
+        commentRepository.save(comment);
+
+        // when & then
+        assertThat(commentRepository.existsByMoment(moment)).isTrue();
+    }
+
+    @Test
+    void Momment의_Comment가_존재하면_false를_반환한다() {
+        // given
+        User momenter = new User("kiki@icloud.com", "1234", "kiki");
+        userRepository.save(momenter);
+
+        Moment moment = new Moment("오늘 하루는 힘든 하루~", true, momenter);
+        momentRepository.save(moment);
+
+        // when & then
+        assertThat(commentRepository.existsByMoment(moment)).isFalse();
     }
 }
