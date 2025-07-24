aba2bcd4570e585451f7d8b134ed25f58c3bae12
haku <hakukudh@gmail.com>
2025-07-24 18:45:51 +0900

    feat: MyMomentList 조건부 렌더링 추가

--- client/src/features/moment/ui/MyMomentsList.tsx
+++ client/src/features/moment/ui/MyMomentsList.tsx
@@ -7,41 +7,50 @@ import { Send, Timer } from 'lucide-react';
 import { useMomentsQuery } from '../hook/useMomentsQuery';
 import { MyMoments } from '../types/moments';
 import * as S from './MyMomentsList.styles';
+import { NotFoundMyMoments } from './NotFoundMyMoments';
 
 export const MyMomentsList = () => {
   const { data } = useMomentsQuery();
   const myMoments = data?.data;
 
+  const hasMoments = myMoments?.length && myMoments.length > 0;
+
   return (
-    <S.MomentsContainer>
-      {myMoments?.map((myMoment: MyMoments, index: number) => (
-        <Card width="large" key={index}>
-          {/* TODO: 추후 key값에 id 값으로 변경 필요 */}
-          <Card.TitleContainer
-            title={
-              <S.TitleWrapper>
-                {/* TODO: 추후 Icon 컴포넌트로 변경 필요 */}
-                <Timer size={16} color={theme.colors['gray-400']} />{' '}
-                <S.TimeStamp>{formatRelativeTime(myMoment.createdAt)}</S.TimeStamp>
-              </S.TitleWrapper>
-            }
-            subtitle={myMoment.content}
-          />
-          <Card.Content>
-            <S.TitleContainer>
-              <Send size={20} color={theme.colors['yellow-500']} />
-              <span>받은 공감</span>
-            </S.TitleContainer>
-            <SimpleCard
-              height="small"
-              content={myMoment.comment?.content || <NotFoundComments />}
-            />
-          </Card.Content>
-          <Card.Action position="space-between">
-            <EmojiButton />
-          </Card.Action>
-        </Card>
-      ))}
-    </S.MomentsContainer>
+    <>
+      {hasMoments ? (
+        <S.MomentsContainer>
+          {myMoments?.map((myMoment: MyMoments, index: number) => (
+            <Card width="large" key={index}>
+              {/* TODO: 추후 key값에 id 값으로 변경 필요 */}
+              <Card.TitleContainer
+                title={
+                  <S.TitleWrapper>
+                    {/* TODO: 추후 Icon 컴포넌트로 변경 필요 */}
+                    <Timer size={16} color={theme.colors['gray-400']} />{' '}
+                    <S.TimeStamp>{formatRelativeTime(myMoment.createdAt)}</S.TimeStamp>
+                  </S.TitleWrapper>
+                }
+                subtitle={myMoment.content}
+              />
+              <Card.Content>
+                <S.TitleContainer>
+                  <Send size={20} color={theme.colors['yellow-500']} />
+                  <span>받은 공감</span>
+                </S.TitleContainer>
+                <SimpleCard
+                  height="small"
+                  content={myMoment.comment?.content || <NotFoundComments />}
+                />
+              </Card.Content>
+              <Card.Action position="space-between">
+                <EmojiButton />
+              </Card.Action>
+            </Card>
+          ))}
+        </S.MomentsContainer>
+      ) : (
+        <NotFoundMyMoments />
+      )}
+    </>
   );
 };
