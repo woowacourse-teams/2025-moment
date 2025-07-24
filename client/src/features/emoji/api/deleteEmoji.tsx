2647c17e02568331442b35cda6c683ed68859804
haku <hakukudh@gmail.com>
2025-07-24 11:08:04 +0900

    chore: 불필요한 파일 삭제

--- client/src/features/emoji/api/deleteEmoji.tsx
+++
@@ -1,6 +0,0 @@
-import { api } from '@/app/lib/api';
-
-export const deleteEmoji = async (emojiId: number) => {
-  const response = await api.delete(`/emojis/${emojiId}`);
-  return response.data;
-};
