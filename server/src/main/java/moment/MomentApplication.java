f4b9bf296626c7dbca7a840a8df6063e21580081
DoubleKK <138849238+kwonkeonhyeong@users.noreply.github.com>
2025-07-24 16:12:11 +0900

    fix: CD .yml 설정 변경 (#182)

--- server/src/main/java/moment/MomentApplication.java
+++ server/src/main/java/moment/MomentApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class MomentApplication {
-    //    CI/CD TEST
     public static void main(String[] args) {
         SpringApplication.run(MomentApplication.class, args);
     }
