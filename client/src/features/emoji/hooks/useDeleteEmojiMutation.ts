919e9dbb66ac3275668629c57eb1858a225287d9
haku <hakukudh@gmail.com>
2025-07-24 13:50:42 +0900

    feat: delete api 로직 구현

---
+++ client/src/features/emoji/hooks/useDeleteEmojiMutation.ts
@@ -0,0 +1,12 @@
+import { useMutation } from '@tanstack/react-query';
+import { deleteEmoji } from '../api/deleteEmoji';
+import { queryClient } from '@/app/lib/queryClient';
+
+export const useDeleteEmojiMutation = () => {
+  return useMutation({
+    mutationFn: deleteEmoji,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['emojis'] });
+    },
+  });
+};
