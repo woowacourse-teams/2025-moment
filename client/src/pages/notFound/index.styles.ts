dc3bf519e91b396aa13b710375420e884fbb719f
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-25 14:54:54 +0900

    style: notfound 페이지 style구현

---
+++ client/src/pages/notFound/index.styles.ts
@@ -0,0 +1,41 @@
+import styled from '@emotion/styled';
+
+export const NotFoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 60vh;
+  padding: 2rem;
+`;
+
+export const NotFoundContent = styled.div`
+  text-align: center;
+  max-width: 500px;
+`;
+
+export const ErrorCode = styled.h1`
+  font-size: 6rem;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors['yellow-500']};
+  margin: 0;
+  line-height: 1;
+`;
+
+export const ErrorTitle = styled.h2`
+  font-size: 1.5rem;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors['red-500']};
+  margin: 1rem 0;
+`;
+
+export const ErrorDescription = styled.p`
+  font-size: 1rem;
+  color: ${({ theme }) => theme.colors['gray-600']};
+  line-height: 1.6;
+  margin: 1rem 0 2rem;
+`;
+
+export const ButtonContainer = styled.div`
+  margin-top: 2rem;
+`;
