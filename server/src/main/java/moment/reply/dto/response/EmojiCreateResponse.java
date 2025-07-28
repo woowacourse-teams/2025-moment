5c0f2226841566d03ec8102638684c5067c2825e
Firedrago95 <yonghwa95@naver.com>
2025-07-28 14:33:29 +0900

    refactor: 이모지 타입 변수명 변경

--- server/src/main/java/moment/reply/dto/response/EmojiCreateResponse.java
+++ server/src/main/java/moment/reply/dto/response/EmojiCreateResponse.java
@@ -3,7 +3,7 @@ package moment.reply.dto.response;
 import java.time.LocalDateTime;
 import moment.reply.domain.Emoji;
 
-public record EmojiCreateResponse(String enumType, Long commentId, Long userId, LocalDateTime createdAt) {
+public record EmojiCreateResponse(String emojiType, Long commentId, Long userId, LocalDateTime createdAt) {
 
     public static EmojiCreateResponse from(Emoji emoji) {
         return new EmojiCreateResponse(
