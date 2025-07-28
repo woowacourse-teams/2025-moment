c929659b2ef83865c278135e0c773dedce75a00c
mimi <110226866+hongkikii@users.noreply.github.com>
2025-07-28 14:19:50 +0900

    refactor: 도메인 예외 처리 로직 수정

--- server/src/main/java/moment/user/domain/User.java
+++ server/src/main/java/moment/user/domain/User.java
@@ -55,7 +55,7 @@ public class User extends BaseEntity {
             throw new IllegalArgumentException("email이 null이거나 빈 값이어서는 안 됩니다.");
         }
         if (!email.matches(EMAIL_REGEX)) {
-            throw new MomentException(ErrorCode.EMAIL_INVALID);
+            throw new IllegalArgumentException("email의 형식이 올바르지 않습니다.");
         }
     }
 
