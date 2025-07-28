826d0fbe46afe6bfa546d97e860aa67a62289782
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-29 03:55:50 +0900

    feat: ProtectedRoute 적용

--- client/src/app/routes/index.tsx
+++ client/src/app/routes/index.tsx
@@ -1,16 +1,17 @@
 import { Layout } from '@/app/layout/ui';
+import { ProtectedRoute } from '@/app/routes/ProtectedRoute';
 import { ROUTES } from '@/app/routes/routes';
 import HomePage from '@/pages/home';
-import MyMoments from '@/pages/myMoments';
 import LoginPage from '@/pages/login';
+import MyMoments from '@/pages/myMoments';
 import SignupPage from '@/pages/signup';
 
-import TodayMomentPage from '@/pages/todayMoment';
 import PostCommentsPage from '@/pages/postComments';
+import TodayMomentPage from '@/pages/todayMoment';
 
-import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router';
 import TodayCommentPage from '@/pages/todayComment/TodayCommentPage';
 import TodayCommentSuccessPage from '@/pages/todayComment/TodayCommentSuccessPage';
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -18,12 +19,13 @@ export const router = createBrowserRouter(
       <Route index element={<HomePage />} />
       <Route path={ROUTES.SIGNUP} element={<SignupPage />} />
       <Route path={ROUTES.LOGIN} element={<LoginPage />} />
-      <Route path={ROUTES.MY_MOMENTS} element={<MyMoments />} />
-      <Route path={ROUTES.TODAY_MOMENT} element={<TodayMomentPage />} />
-      <Route path={ROUTES.POST_COMMENTS} element={<PostCommentsPage />} />
-      <Route path={ROUTES.TODAY_MOMENT} element={<TodayMomentPage />} />
-      <Route path={ROUTES.TODAY_COMMENT} element={<TodayCommentPage />} />
-      <Route path={ROUTES.TODAY_COMMENT_SUCCESS} element={<TodayCommentSuccessPage />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path={ROUTES.MY_MOMENTS} element={<MyMoments />} />
+        <Route path={ROUTES.TODAY_MOMENT} element={<TodayMomentPage />} />
+        <Route path={ROUTES.POST_COMMENTS} element={<PostCommentsPage />} />
+        <Route path={ROUTES.TODAY_COMMENT} element={<TodayCommentPage />} />
+        <Route path={ROUTES.TODAY_COMMENT_SUCCESS} element={<TodayCommentSuccessPage />} />
+      </Route>
     </Route>,
   ),
 );
