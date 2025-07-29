d3ac6b29437e6baea671d215c3d718a3a570198f
이상희 <sang6299@naver.com>
2025-07-29 11:16:47 +0900

    refactor: 이메일, 닉네임 중복 체크 API 엔드포인트 변경

--- client/src/features/auth/api/checkNicknameExist.ts
+++ client/src/features/auth/api/checkNicknameExist.ts
@@ -1,6 +1,6 @@
 import { api } from '@/app/lib/api';
 
 export const checkNicknameExist = async (nickname: string) => {
-  const response = await api.post('/users/nickname/check', nickname);
+  const response = await api.post('/users/signup/nickname/check', nickname);
   return response.data;
 };
