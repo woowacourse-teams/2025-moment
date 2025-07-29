2bc8d7fcd1330905034df2d63a3853478fb85f74
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-29 18:25:55 +0900

    feat: NotFound 공통 컴포넌트 구현

---
+++ client/src/shared/ui/notFound/NotFound.tsx
@@ -0,0 +1,39 @@
+import { Eye, LucideIcon } from 'lucide-react';
+import { Card } from '../';
+import * as S from './NotFound.styles';
+
+interface NotFoundProps {
+  title: string;
+  subtitle: string;
+  icon?: LucideIcon;
+  iconSize?: number;
+  size?: 'small' | 'large';
+  withCard?: boolean;
+}
+
+export const NotFound = ({
+  title,
+  subtitle,
+  icon: IconComponent = Eye,
+  iconSize = 24,
+  size = 'large',
+  withCard = false,
+}: NotFoundProps) => {
+  const content = (
+    <S.NotFoundWrapper size={size}>
+      <S.NotFoundIconWrapper size={size}>
+        <IconComponent size={iconSize} />
+      </S.NotFoundIconWrapper>
+      <S.NotFoundContainer size={size}>
+        <S.NotFoundTitle size={size}>{title}</S.NotFoundTitle>
+        <S.NotFoundSubtitle size={size}>{subtitle}</S.NotFoundSubtitle>
+      </S.NotFoundContainer>
+    </S.NotFoundWrapper>
+  );
+
+  if (withCard) {
+    return <Card width="medium">{content}</Card>;
+  }
+
+  return content;
+};
