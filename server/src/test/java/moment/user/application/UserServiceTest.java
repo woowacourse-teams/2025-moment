e6939da5af540459fc38d4b09375536ef316155e
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-07-29 13:30:36 +0900

    fix: 필드 중복 선언 문제 해결

--- server/src/test/java/moment/user/application/UserServiceTest.java
+++ server/src/test/java/moment/user/application/UserServiceTest.java
@@ -3,12 +3,11 @@ package moment.user.application;
 import moment.global.exception.ErrorCode;
 import moment.global.exception.MomentException;
 import moment.user.domain.User;
+import moment.user.dto.request.EmailConflictCheckRequest;
 import moment.user.dto.request.NicknameConflictCheckRequest;
 import moment.user.dto.request.UserCreateRequest;
-import moment.user.dto.response.NicknameConflictCheckResponse;
-import moment.user.dto.request.EmailConflictCheckRequest;
-import moment.user.dto.request.UserCreateRequest;
 import moment.user.dto.response.EmailConflictCheckResponse;
+import moment.user.dto.response.NicknameConflictCheckResponse;
 import moment.user.infrastructure.UserRepository;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
@@ -26,18 +25,12 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.times;
-
 @ExtendWith(MockitoExtension.class)
 @DisplayNameGeneration(ReplaceUnderscores.class)
 class UserServiceTest {
 
     private final UserCreateRequest request = new UserCreateRequest("mimi@icloud.com", "mimi1234", "mimi1234", "미미");
+
     @InjectMocks
     private UserService userService;
     @Mock
@@ -45,8 +38,6 @@ class UserServiceTest {
 
     @Mock
     private PasswordEncoder passwordEncoder;
-
-    private final UserCreateRequest request = new UserCreateRequest("mimi@icloud.com", "mimi1234", "mimi1234", "미미");
 
     @Test
     void 유저_생성에_성공한다() {
@@ -86,10 +77,10 @@ class UserServiceTest {
 
     @Test
     void 비밀번호와_비밀번호_확인_값이_일치하지_않는_경우_예외가_발생한다() {
-        UserCreateRequest invalidRequest = new UserCreateRequest("mimi@icloud.com", "mimi1234", "mimi5678", "미미");
+        UserCreateRequest request = new UserCreateRequest("mimi@icloud.com", "mimi1234", "mimi5678", "미미");
 
         // when & then
-        assertThatThrownBy(() -> userService.addUser(invalidRequest))
+        assertThatThrownBy(() -> userService.addUser(request))
                 .isInstanceOf(MomentException.class)
                 .hasFieldOrPropertyWithValue("errorCode", ErrorCode.PASSWORD_MISMATCHED);
     }
@@ -107,7 +98,7 @@ class UserServiceTest {
         // & then
         assertThat(response.isExists()).isTrue();
     }
-  
+
     @Test
     void 이미_존재하는_이메일일_경우_true를_반환한다() {
         // given
@@ -121,7 +112,7 @@ class UserServiceTest {
         // & then
         assertThat(response.isExists()).isTrue();
     }
-  
+
     @Test
     void 이미_존재하는_닉네임이_아닐_경우_false를_반환한다() {
         // given
@@ -131,11 +122,11 @@ class UserServiceTest {
 
         // when
         NicknameConflictCheckResponse response = userService.checkNicknameConflict(request);
-      
+
         // & then
         assertThat(response.isExists()).isFalse();
     }
-  
+
     @Test
     void 이미_존재하는_이메일이_아닐_경우_false를_반환한다() {
         // given
