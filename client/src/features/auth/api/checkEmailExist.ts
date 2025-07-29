61e6efbe4582de6482e48a82a06c4dc2a87d64ab
이상희 <sang6299@naver.com>
2025-07-29 13:49:09 +0900

    fix: 중복 체크 API 요청시 객체에 담겨 보내도록 수정

--- client/src/features/auth/api/checkEmailExist.ts
+++ client/src/features/auth/api/checkEmailExist.ts
@@ -1,6 +1,6 @@
 import { api } from '@/app/lib/api';
 
 export const checkEmailExist = async (email: string) => {
-  const response = await api.post('/users/signup/email/check', email);
+  const response = await api.post('/users/signup/email/check', { email });
   return response.data;
 };
