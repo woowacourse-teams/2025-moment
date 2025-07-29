7415a90bc903ecdc02e272d7a19cf21938df1162
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-29 12:47:24 +0900

    fix: 오늘의 모멘트 로직 수정

--- client/src/features/moment/ui/TodayMomentForm.tsx
+++ client/src/features/moment/ui/TodayMomentForm.tsx
@@ -1,25 +1,23 @@
 import { useSendMoments } from '@/features/moment/hook/useSendMoments';
 import { TodayMomentWriteContent } from '@/features/moment/ui/TodayMomentWriteContent';
 import { Card } from '@/shared/ui';
-import { TodayMomentAllowedContent } from './TodayMomentAllowedContent';
 import { useCheckMomentsQuery } from '../hook/useCheckMomentsQuery';
+import { TodayMomentAllowedContent } from './TodayMomentAllowedContent';
 
 export function TodayMomentForm() {
   const { handleContentChange, handleSendContent, content } = useSendMoments();
   const { data: checkMomentsData } = useCheckMomentsQuery();
 
-  const isAllowed = checkMomentsData?.data?.status === 'ALLOWED';
-
   return (
     <Card width="medium">
-      {isAllowed ? (
-        <TodayMomentAllowedContent />
-      ) : (
+      {checkMomentsData?.data?.status === 'ALLOWED' ? (
         <TodayMomentWriteContent
           handleContentChange={handleContentChange}
           handleSendContent={handleSendContent}
           content={content}
         />
+      ) : (
+        <TodayMomentAllowedContent />
       )}
     </Card>
   );
