875e7afa01754370a00be6884e5f0cf92c3695c9
yush <yush2202@gmail.com>
2025-07-29 11:34:47 +0900

    docs: response 문서화

--- server/src/main/java/moment/moment/dto/response/MomentCreationStatusResponse.java
+++ server/src/main/java/moment/moment/dto/response/MomentCreationStatusResponse.java
@@ -1,7 +1,12 @@
 package moment.moment.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import moment.moment.domain.MomentCreationStatus;
 
-public record MomentCreationStatusResponse(MomentCreationStatus status) {
-
+@Schema(description = "모멘트 생성 가능 여부 상태")
+public record MomentCreationStatusResponse(
+        @Schema(description = "모멘트 생성 가능 여부 상태", example = "ALLOWED")
+        MomentCreationStatus status
+) {
+    
 }
