95dbf96fa75eced2a4f1a9ccc03bee87f333e75e
haku <hakukudh@gmail.com>
2025-07-29 11:15:22 +0900

    feat:  api URL 변경

--- client/src/features/moment/api/getCheckMoments.ts
+++ client/src/features/moment/api/getCheckMoments.ts
@@ -2,6 +2,6 @@ import { api } from '@/app/lib/api';
 import { CheckMomentsResponse } from '../types/moments';
 
 export const getCheckMoments = async (): Promise<CheckMomentsResponse> => {
-  const response = await api.get('/moments/creation-status');
+  const response = await api.get('moments/me/creation-status');
   return response.data;
 };
