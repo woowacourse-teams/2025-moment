ef129f8675a7214d8f57d80e0cac6f29c1b27218
mimi <110226866+hongkikii@users.noreply.github.com>
2025-07-30 19:11:39 +0900

    feat: 로깅 형식 및 파일 설정

--- server/src/main/java/moment/global/logging/ApiLogInterceptor.java
+++ server/src/main/java/moment/global/logging/ApiLogInterceptor.java
@@ -3,6 +3,7 @@ package moment.global.logging;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,18 +14,20 @@ import java.util.UUID;
 @Component
 public class ApiLogInterceptor implements HandlerInterceptor {
 
+    private static final String TRACE_ID_KEY = "traceId";
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String requestURI = request.getRequestURI();
-        String uuid = UUID.randomUUID().toString().substring(0, 8);
-        request.setAttribute("logId", uuid);
-        log.info("uuid: [{}], ip: [{}], method: [{}], uri: [{}]", uuid, request.getRemoteAddr(), request.getMethod(), requestURI);
+        String traceId = UUID.randomUUID().toString().substring(0, 8);
+        MDC.put(TRACE_ID_KEY, traceId);
+        log.info("ip: [{}], method: [{}], uri: [{}]", request.getRemoteAddr(), request.getMethod(), requestURI);
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        String uuid = (String) request.getAttribute("logId");
-        log.info("uuid: [{}], status: [{}]", uuid, response.getStatus());
+        log.info("status: [{}]", response.getStatus());
+        MDC.remove(TRACE_ID_KEY);
     }
 }
