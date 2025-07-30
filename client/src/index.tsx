10da8544ddd43d35b9b8e8b2a40ff0eddd8cbcb5
eunwoo-levi <eunwoo1341@gmail.com>
2025-07-30 17:54:37 +0900

    fix: build test

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -15,6 +15,7 @@ async function enableMocking() {
 
 async function startApp() {
   // await enableMocking();
+  // test
 
   const rootElement = document.getElementById('root');
   if (!rootElement) {
