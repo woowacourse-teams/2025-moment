c62877d61849893b4746531dc96167230d93e8a4
Firedrago95 <yonghwa95@naver.com>
2025-07-31 20:07:20 +0900

    feat: 레포지토리 SQL 로그 출력 기능 구현

--- server/src/main/java/moment/global/logging/ApiLogInterceptor.java
+++ server/src/main/java/moment/global/logging/ApiLogInterceptor.java
@@ -21,6 +21,7 @@ public class ApiLogInterceptor implements HandlerInterceptor {
         String requestURI = request.getRequestURI();
         String traceId = UUID.randomUUID().toString().substring(0, 8);
         MDC.put(TRACE_ID_KEY, traceId);
+        log.info("======================================================================");
         log.info("ip: [{}], method: [{}], uri: [{}]", request.getRemoteAddr(), request.getMethod(), requestURI);
         return true;
     }
@@ -29,6 +30,7 @@ public class ApiLogInterceptor implements HandlerInterceptor {
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
         String requestURI = request.getRequestURI();
         log.info("ip: [{}], method: [{}], uri: [{}], status: [{}]", request.getRemoteAddr(), request.getMethod(), requestURI, response.getStatus());
+        log.info("======================================================================");
         MDC.remove(TRACE_ID_KEY);
     }
 }
