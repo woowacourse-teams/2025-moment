b1ac430c1206a1e3c0414c3d9ea3c98e1a02fbf7
Firedrago95 <yonghwa95@naver.com>
2025-08-02 16:31:59 +0900

    refactor: ci 멀티 플랫폼 설정 제거

--- server/src/main/java/moment/MomentApplication.java
+++ server/src/main/java/moment/MomentApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class MomentApplication {
     public static void main(String[] args) {
+        //
         SpringApplication.run(MomentApplication.class, args);
     }
 }
