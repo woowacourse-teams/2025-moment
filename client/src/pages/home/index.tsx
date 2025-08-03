ca6d6e92a673e82d9f1b6645a3bb3920cc21354b
이상희 <sang6299@naver.com>
2025-08-03 17:31:00 +0900

    feat: 랜딩 페이지 버튼에도 Hero 애니메이션 적용

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -2,11 +2,13 @@ import { ROUTES } from '@/app/routes/routes';
 import { sendEvent } from '@/shared/lib/ga';
 import { Button } from '@/shared/ui/button/Button';
 import { Hero } from '@/widgets/hero';
+import { useDelayedVisible } from '@/shared/hooks/useDelayedVisible';
 import { useNavigate } from 'react-router';
 import * as S from './index.styles';
 
 export default function HomePage() {
   const navigate = useNavigate();
+  const { isVisible } = useDelayedVisible({ delay: 100 });
 
   const handleClick = () => {
     sendEvent({
@@ -22,7 +24,7 @@ export default function HomePage() {
       <S.HeroSection>
         <Hero />
       </S.HeroSection>
-      <S.ContentSection>
+      <S.ContentSection isVisible={isVisible}>
         <Button title="모멘트 작성하기" variant="secondary" onClick={handleClick} />
       </S.ContentSection>
     </S.HomePageWrapper>
