c80ace6b2372f0287cba3f28a1a40ce9295a5289
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-08-05 22:06:37 +0900

    fix: MomentControllerTest 수정

--- server/src/test/java/moment/moment/presentation/MomentControllerTest.java
+++ server/src/test/java/moment/moment/presentation/MomentControllerTest.java
@@ -127,6 +127,53 @@ class MomentControllerTest {
     }
 
     @Test
+//    @Disabled
+    void DB에_저장된_Moment가_limit보다_적을_경우_남은_목록을_반환한다() throws InterruptedException {
+        // given
+        User momenter = new User("hippo@gmail.com", "1234", "hippo", ProviderType.EMAIL);
+        User savedMomenter = userRepository.save(momenter);
+
+        String token = tokenManager.createToken(savedMomenter.getId(), savedMomenter.getEmail());
+
+        Moment moment1 = new Moment("아 행복해", true, savedMomenter);
+        Moment moment2 = new Moment("아 힘들어", true, savedMomenter);
+        Moment moment3 = new Moment("아 짜증나", false, savedMomenter);
+        Moment moment4 = new Moment("아 신기해", false, savedMomenter);
+
+        momentRepository.save(moment1);
+        Thread.sleep(200);
+        Moment cursorMoment = momentRepository.save(moment2);
+        Thread.sleep(200);
+        momentRepository.save(moment3);
+        Thread.sleep(200);
+        momentRepository.save(moment4);
+
+        // when
+        MyMomentPageResponse response = RestAssured.given().log().all()
+                .param("limit", 10)
+                .cookie("token", token)
+                .when().get("/api/v1/moments/me")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .jsonPath()
+                .getObject("data", MyMomentPageResponse.class);
+
+        // then
+        assertAll(
+                () -> assertThat(response.items()).hasSize(4),
+                () -> assertThat(response.items().stream()
+                        .allMatch(item -> item.momenterId().equals(savedMomenter.getId())))
+                        .isTrue(),
+                () -> assertThat(response.items())
+                        .isSortedAccordingTo(Comparator.comparing(MyMomentResponse::createdAt).reversed()),
+                () -> assertThat(response.nextCursor()).isNull(),
+                () -> assertThat(response.hasNextPage()).isFalse(),
+                () -> assertThat(response.pageSize()).isEqualTo(4)
+        );
+    }
+
+    @Test
     void 매칭된_모멘트를_조회한다() {
         // given
         User momenter = new User("hippo@gmail.com", "1234", "hippo", ProviderType.EMAIL);
