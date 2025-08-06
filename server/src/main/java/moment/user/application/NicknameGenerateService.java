982f60d802d214fce6e45183842e4d51bfedc7bf
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-08-06 16:09:35 +0900

    refactor: 매직넘버 상수처리

--- server/src/main/java/moment/user/application/NicknameGenerateService.java
+++ server/src/main/java/moment/user/application/NicknameGenerateService.java
@@ -12,14 +12,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class NicknameGenerateService {
 
+    private static final int LIMIT_RETRY_COUNT = 5;
+
     private final UserQueryService userQueryService;
     private final NicknameGenerator nicknameGenerator;
 
     public String createRandomNickname() {
-        int tryCount = 0;
+        int retry = 0;
 
         while (true) {
-            if (tryCount > 5) {
+            if (retry > LIMIT_RETRY_COUNT) {
                 throw new MomentException(ErrorCode.USER_NICKNAME_GENERATION_FAILED);
             }
 
@@ -29,7 +31,7 @@ public class NicknameGenerateService {
                 return nickname;
             }
 
-            tryCount++;
+            retry++;
         }
     }
 }
