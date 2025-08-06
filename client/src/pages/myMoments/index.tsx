d39d534611fed05037c85e15065cb376b0f6edfe
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-06 15:09:01 +0900

    chore: index.styles.ts 제거 후 컴포넌트에 style 추가

--- client/src/pages/myMoments/index.tsx
+++ client/src/pages/myMoments/index.tsx
@@ -1,15 +1,22 @@
 import { MyMomentsList } from '@/features/moment/ui/MyMomentsList';
 import { TitleContainer } from '@/shared/ui/titleContainer/TitleContainer';
-import * as S from './index.styles';
+import styled from '@emotion/styled';
 
 export default function MyMoments() {
   return (
-    <S.MyMomentsPageContainer>
+    <MyMomentsPageContainer>
       <TitleContainer
         title="나의 모멘트"
         subtitle="내가 공유한 모멘트와 받은 공감을 확인해보세요"
       />
       <MyMomentsList />
-    </S.MyMomentsPageContainer>
+    </MyMomentsPageContainer>
   );
 }
+
+export const MyMomentsPageContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  margin: 20px;
+`;
