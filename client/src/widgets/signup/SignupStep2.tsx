1325d5e39f61812665883d31ef1ca631b070ff5f
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-07 18:55:41 +0900

    feat: nickname 랜덤받기 기능 추가

--- client/src/widgets/signup/SignupStep2.tsx
+++ client/src/widgets/signup/SignupStep2.tsx
@@ -2,6 +2,8 @@ import { useRandomNicknameQuery } from '@/features/auth/hooks/useRandomNicknameQ
 import { SignupFormData } from '@/features/auth/types/signup';
 import { useEnterKeyHandler } from '@/shared/hooks/useEnterKeyHandler';
 import { Input } from '@/shared/ui/input/Input';
+import styled from '@emotion/styled';
+import { RotateCw } from 'lucide-react';
 import { useEffect } from 'react';
 import * as S from './SignupStep.styles';
 
@@ -13,7 +15,7 @@ interface SignupStep2Props {
 
 export const SignupStep2 = ({ signupData, onNext, updateNickname }: SignupStep2Props) => {
   useEnterKeyHandler(onNext);
-  const { data: randomNickname, isError, error } = useRandomNicknameQuery();
+  const { data: randomNickname, isError, error, refetch } = useRandomNicknameQuery();
 
   useEffect(() => {
     if (randomNickname) {
@@ -24,6 +26,10 @@ export const SignupStep2 = ({ signupData, onNext, updateNickname }: SignupStep2P
   if (isError) {
     console.error('Error fetching random nickname:', error);
   }
+
+  const handleRotateNickname = () => {
+    refetch();
+  };
 
   return (
     <S.StepContainer>
@@ -37,6 +43,9 @@ export const SignupStep2 = ({ signupData, onNext, updateNickname }: SignupStep2P
             value={signupData.nickname}
             disabled
           />
+          <RotateNicknameButton onClick={handleRotateNickname}>
+            <RotateCw size={25} color="white" />
+          </RotateNicknameButton>
         </S.CheckExistContainer>
         {isError && (
           <S.ErrorMessage>닉네임을 가져오는 데 실패했습니다. 다시 시도해주세요.</S.ErrorMessage>
@@ -45,3 +54,17 @@ export const SignupStep2 = ({ signupData, onNext, updateNickname }: SignupStep2P
     </S.StepContainer>
   );
 };
+
+export const RotateNicknameButton = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    scale: 1.1;
+    transition: scale 0.1s ease-in-out;
+  }
+`;
