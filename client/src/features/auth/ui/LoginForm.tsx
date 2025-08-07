eb71a38de00aae9c3f585ccf8fca8bc2b1fd084c
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-07 18:01:45 +0900

    refactor: LoginForm 구조 변경하여 구글로그인 클릭 시 오류메시지 나오는 문제 해결

--- client/src/features/auth/ui/LoginForm.tsx
+++ client/src/features/auth/ui/LoginForm.tsx
@@ -1,8 +1,8 @@
 import { useLoginForm } from '@/features/auth/hooks/useLoginForm';
 import { Input } from '@/shared/ui/input/Input';
 import { useNavigate } from 'react-router';
-import * as S from './LoginForm.styles';
 import { GoogleLoginButton } from './GoogleLoginButton';
+import * as S from './LoginForm.styles';
 
 export const LoginForm = () => {
   const navigate = useNavigate();
@@ -19,43 +19,45 @@ export const LoginForm = () => {
   };
 
   return (
-    <S.LoginFormWrapper onSubmit={handleSubmit}>
-      <S.LoginFormTitleContainer>
-        <S.LoginLogoTitleContainer>
-          <S.LogoImage src="/logo.webp" alt="Moment Logo Image" />
-          <S.LogoTitle>모멘트</S.LogoTitle>
-        </S.LoginLogoTitleContainer>
-        <S.LoginTitle>로그인</S.LoginTitle>
-        <S.LoginDescription>이메일과 비밀번호를 입력하여 로그인해주세요.</S.LoginDescription>
-      </S.LoginFormTitleContainer>
-      <S.LoginFormContent>
-        <S.InputGroup>
-          <S.Label htmlFor="email">이메일</S.Label>
-          <Input
-            id="email"
-            type="email"
-            placeholder="이메일을 입력해주세요"
-            value={formData.email}
-            onChange={handleChange('email')}
-          />
-          <S.ErrorMessage>{errors.email || ' '}</S.ErrorMessage>
-        </S.InputGroup>
-        <S.InputGroup>
-          <S.Label htmlFor="password">비밀번호</S.Label>
-          <Input
-            id="password"
-            type="password"
-            placeholder="비밀번호를 입력해주세요"
-            value={formData.password}
-            onChange={handleChange('password')}
-          />
-          <S.ErrorMessage>{errors.password || ' '}</S.ErrorMessage>
-        </S.InputGroup>
-      </S.LoginFormContent>
-      <S.LoginFooter>
+    <S.LoginFormWrapper>
+      <S.LoginFormContainer onSubmit={handleSubmit}>
+        <S.LoginFormTitleContainer>
+          <S.LoginLogoTitleContainer>
+            <S.LogoImage src="/logo.webp" alt="Moment Logo Image" />
+            <S.LogoTitle>모멘트</S.LogoTitle>
+          </S.LoginLogoTitleContainer>
+          <S.LoginTitle>로그인</S.LoginTitle>
+          <S.LoginDescription>이메일과 비밀번호를 입력하여 로그인해주세요.</S.LoginDescription>
+        </S.LoginFormTitleContainer>
+        <S.LoginFormContent>
+          <S.InputGroup>
+            <S.Label htmlFor="email">이메일</S.Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="이메일을 입력해주세요"
+              value={formData.email}
+              onChange={handleChange('email')}
+            />
+            <S.ErrorMessage>{errors.email || ' '}</S.ErrorMessage>
+          </S.InputGroup>
+          <S.InputGroup>
+            <S.Label htmlFor="password">비밀번호</S.Label>
+            <Input
+              id="password"
+              type="password"
+              placeholder="비밀번호를 입력해주세요"
+              value={formData.password}
+              onChange={handleChange('password')}
+            />
+            <S.ErrorMessage>{errors.password || ' '}</S.ErrorMessage>
+          </S.InputGroup>
+        </S.LoginFormContent>
         <S.LoginButton type="submit" disabled={isDisabled}>
           로그인
         </S.LoginButton>
+      </S.LoginFormContainer>
+      <S.LoginFooter>
         <GoogleLoginButton onClick={handleGoogleLogin} />
         <S.LoginFooterContent>
           <S.LoginForgotPassword>비밀번호를 잊으셨나요?</S.LoginForgotPassword>
