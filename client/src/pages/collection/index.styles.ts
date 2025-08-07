2e2ae48e3632121e5386ef4f3f2ffd0f10736071
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-07 12:52:54 +0900

    style: Collection style 구현 및 적용

--- client/src/pages/collection/index.styles.ts
+++ client/src/pages/collection/index.styles.ts
@@ -1,15 +1,52 @@
 import styled from '@emotion/styled';
+import { Link } from 'react-router';
 
 export const CollectionContainer = styled.section`
   display: flex;
   flex-direction: column;
-  gap: 30px;
+  gap: 50px;
   margin: 20px;
+
+  @media (max-width: 768px) {
+    gap: 30px;
+  }
 `;
 
-export const SelectButtonContainer = styled.div`
+export const CollectionHeaderContainer = styled.div`
+  width: 60%;
   display: flex;
   align-items: center;
   justify-content: center;
   gap: 60px;
+  margin: 0 auto;
+  border: 1px solid #ccc;
+  padding: 20px;
+
+  @media (max-width: 768px) {
+    width: 90%;
+  }
+`;
+
+export const CollectionHeaderLinkContainer = styled(Link)`
+  color: white;
+  font-size: 1.5rem;
+  font-weight: bold;
+
+  &.active {
+    font-size: 1.8rem;
+    color: yellow;
+  }
+
+  &:hover {
+    scale: 1.05;
+    transition: all 0.2s ease-in-out;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 1.2rem;
+
+    &.active {
+      font-size: 1.4rem;
+    }
+  }
 `;
--- client/src/pages/collection/mymoment/index.tsx
+++ client/src/pages/collection/mymoment/index.tsx
@@ -1,16 +1,12 @@
-import { ROUTES } from '@/app/routes/routes';
 import { MyMomentsList } from '@/features/moment/ui/MyMomentsList';
+import { CollectionHeader } from '@/pages/collection/CollectionHeader';
 import { TitleContainer } from '@/shared/ui/titleContainer/TitleContainer';
-import { Link } from 'react-router';
 import * as S from '../index.styles';
 
 export default function MyMomentCollectionPage() {
   return (
     <S.CollectionContainer>
-      <S.SelectButtonContainer>
-        <Link to={ROUTES.COLLECTION_MYMOMENT} />
-        <Link to={ROUTES.COLLECTION_MYCOMMENT} />
-      </S.SelectButtonContainer>
+      <CollectionHeader />
       <TitleContainer
         title="나의 모멘트"
         subtitle={'내가 공유한 모멘트와 받은 공감을 확인해보세요'}
