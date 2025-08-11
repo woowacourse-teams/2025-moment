59e6149dbdd5c9b55f562fdeeb92eae3e50e53eb
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-11 10:53:01 +0900

    build: global 변수 추가

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -58,6 +58,8 @@ export default [
         setInterval: 'readonly',
         clearInterval: 'readonly',
         URLSearchParams: 'readonly',
+        localStorage: 'readonly',
+        sessionStorage: 'readonly',
         // DOM types
         Node: 'readonly',
         Element: 'readonly',
