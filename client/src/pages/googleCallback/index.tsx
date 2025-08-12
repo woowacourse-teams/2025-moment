09673588b903391d2588811923031766a5ab9c30
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-13 01:24:27 +0900

    feat: callback page에서 뒤로가기 못하도록 navigate replace추가

--- client/src/pages/googleCallback/index.tsx
+++ client/src/pages/googleCallback/index.tsx
@@ -20,7 +20,7 @@ export default function GoogleCallbackPage() {
     } else {
       showError('구글 로그인에 실패했습니다. 다시 시도해주세요.');
     }
-    navigate('/');
+    navigate('/', { replace: true });
   }, []);
 
   return <div>Google Callback Page</div>;
