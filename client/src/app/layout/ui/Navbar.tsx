6d9f9ae29a16b1eafd025766d65af2abccc4304f
이상희 <sang6299@naver.com>
2025-08-12 14:25:50 +0900

    feat: 버튼 GA 적용

--- client/src/app/layout/ui/Navbar.tsx
+++ client/src/app/layout/ui/Navbar.tsx
@@ -9,6 +9,7 @@ import { useRef } from 'react';
 import { Link, useLocation } from 'react-router';
 import * as S from './Navbar.styles';
 import { NavigatorsBar } from '@/widgets/navigatorsBar';
+import { sendEvent } from '@/shared/lib/ga';
 
 type Level = 'METEOR' | 'ASTEROID' | 'COMET';
 
@@ -34,6 +35,23 @@ export const Navbar = () => {
     excludeRefs: [dropdownButtonRef],
   });
 
+  const handleDesktopAuthButtonClick = () => {
+    sendEvent({
+      category: 'HomePage',
+      action: 'Click Desktop Auth Button',
+      label: 'Desktop Auth Button',
+    });
+  };
+
+  const handleMobileAuthButtonClick = () => {
+    toggleMobileMenu();
+    sendEvent({
+      category: 'HomePage',
+      action: 'Click Mobile Auth Button',
+      label: 'Mobile Auth Button',
+    });
+  };
+
   return (
     <S.Navbar>
       <Logo />
@@ -42,7 +60,7 @@ export const Navbar = () => {
 
       <S.DesktopAuthButton>
         {profile?.level && <S.LevelIcon src={levelMap[profile?.level as Level]} alt="level" />}
-        <AuthButton />
+        <AuthButton onClick={handleDesktopAuthButtonClick} />
       </S.DesktopAuthButton>
 
       <S.DropdownButton
@@ -63,7 +81,7 @@ export const Navbar = () => {
                 </Link>
               </S.MobileNavItem>
             ))}
-            <AuthButton onClick={toggleMobileMenu} />
+            <AuthButton onClick={handleMobileAuthButtonClick} />
           </S.MobileNavItems>
         </S.MobileMenuContent>
       </S.MobileMenu>
