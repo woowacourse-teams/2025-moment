02a46e6170a6adb7aaf522bb1e08d7cbaaecf097
mimi <110226866+hongkikii@users.noreply.github.com>
2025-08-16 19:38:44 +0900

    refactor: 포인트 관련 변수 Reason에서 재사용

--- server/src/main/java/moment/moment/domain/PointDeductionPolicy.java
+++ server/src/main/java/moment/moment/domain/PointDeductionPolicy.java
@@ -14,6 +14,7 @@ public class PointDeductionPolicy implements ExtraMomentCreatePolicy {
 
     @Override
     public boolean canCreate(User user) {
+        // TODO : user 포인트를 확인할 수 있는 코드가 삽입되면 해당 코드 재사용
         return user.getCurrentPoint() + Reason.MOMENT_ADDITIONAL_USE.getPointTo() >= 0;
     }
 }
