c827550088a2f9b48e4e9fb65ae10fe142c08379
yush <yush2202@gmail.com>
2025-08-17 18:05:56 +0900

    fix: 오타 수정

--- server/src/test/resources/db/migration/h2/V7__create_refreshToken__h2.sql
+++ server/src/test/resources/db/migration/h2/V7__create_refreshToken__h2.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS refresh_tokens
 (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    value VARCHAR(255) NOT NULL,
+    token_value VARCHAR(255) NOT NULL,
     user_id BIGINT NOT NULL,
-    issued_at VARCHAR(255) NOT NULL,
-    expired_at VARCHAR(255) NOT NULL,
+    issued_at DATETIME NOT NULL,
+    expired_at DATETIME NOT NULL,
     created_at TIMESTAMP NOT NULL,
     CONSTRAINT fk_refresh_tokens_users
-    FOREIGN KEY (user_id)
-    REFERENCES users (id)
+        FOREIGN KEY (user_id)
+        REFERENCES users (id)
 );
