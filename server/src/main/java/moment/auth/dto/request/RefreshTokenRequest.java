c6d2a426fc30cad9414825eafeaef004ef1648a0
yush <yush2202@gmail.com>
2025-08-17 23:11:38 +0900

    docs: 요청 swagger 추가

--- server/src/main/java/moment/auth/dto/request/RefreshTokenRequest.java
+++ server/src/main/java/moment/auth/dto/request/RefreshTokenRequest.java
@@ -1,4 +1,10 @@
 package moment.auth.dto.request;
 
-public record RefreshTokenRequest(String refreshToken) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "엑세스 토큰 재발급 요청")
+public record RefreshTokenRequest(
+        @Schema(description = "리프레시 토큰", example = "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3NTU0NDE2MDMsInN1YiI6IjEiLCJlbWFpbCI6ImVrb3JlYTYyM0BnbWFpbC5jb20iLCJpYXQiOjE3NTU0Mzk4MDN9.6Y9r8he8ZOIaXthdxK9ofXtcKbtZH3PXc1WFEjKdCUQf")
+        String refreshToken
+) {
 }
