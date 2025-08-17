621a25ace07163df22c26d0b73286061bbc95af0
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-18 01:02:34 +0900

    feat: 오류 메시지를 alert에서 toast로 변경

--- client/src/app/routes/ProtectedRoute.tsx
+++ client/src/app/routes/ProtectedRoute.tsx
@@ -1,14 +1,16 @@
 import { useCheckIfLoggedInQuery } from '@/features/auth/hooks/useCheckIfLoggedInQuery';
+import { useToast } from '@/shared/hooks';
 import React from 'react';
 import { Navigate, Outlet, useLocation } from 'react-router';
 import { ROUTES } from './routes';
 
 export const ProtectedRoute: React.FC = () => {
   const location = useLocation();
+  const { showError } = useToast();
   const { data: isLoggedIn } = useCheckIfLoggedInQuery();
 
   if (!isLoggedIn) {
-    alert('로그인 후 이용해주세요.');
+    showError('로그인 후 이용해주세요.', 3000);
     return <Navigate to={ROUTES.LOGIN} state={{ from: location }} replace />;
   }
 
