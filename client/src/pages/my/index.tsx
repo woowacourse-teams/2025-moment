0617f7d8ad4eed25a6d62500292def81c3c0789c
haku <hakukudh@gmail.com>
2025-08-17 21:11:21 +0900

    feat: 레벨등급표 추가

--- client/src/pages/my/index.tsx
+++ client/src/pages/my/index.tsx
@@ -1,10 +1,14 @@
 import { mockProfile } from '@/features/my/api/mockData';
 import * as S from './index.styles';
-import { Card } from '@/shared/ui';
+import { Button, Card } from '@/shared/ui';
 import { EXPBar } from '@/widgets/EXPBar/EXPBar';
 import { levelMap } from '@/app/layout/data/navItems';
+import { Modal } from '@/shared/ui/modal/Modal';
+import { LevelTable } from '@/widgets/levelTable/LevelTable';
+import { useState } from 'react';
 
 export default function MyPage() {
+  const [isOpen, setIsOpen] = useState(false);
   const EXPBarProgress =
     (mockProfile.data.expStar / (mockProfile.data.nextStepExp + mockProfile.data.expStar)) * 100;
 
@@ -40,6 +44,7 @@ export default function MyPage() {
                   <span className="separator">/</span>
                   <span className="total">{totalExp}</span>
                 </S.EXPStats>
+                <Button variant="primary" title="레벨 등급표" onClick={() => setIsOpen(true)} />
               </S.EXPBarContainer>
             </S.EXPSection>
           </S.UserInfoContainer>
@@ -62,6 +67,13 @@ export default function MyPage() {
       <S.SettingSection>
         <S.SectionTitle>설정</S.SectionTitle>
       </S.SettingSection>
+
+      <Modal isOpen={isOpen} position="center" size="large" onClose={() => setIsOpen(false)}>
+        <Modal.Header title="레벨 등급표" />
+        <Modal.Content>
+          <LevelTable />
+        </Modal.Content>
+      </Modal>
     </S.MyPageWrapper>
   );
 }
