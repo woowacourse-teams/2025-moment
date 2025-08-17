0dc56c05df06d024dc2a11f9656566b088aac1e6
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-18 00:27:07 +0900

    style: NavigatorsBarContainer에 shadow 스타일 추가

--- client/src/widgets/navigatorsBar/index.styles.ts
+++ client/src/widgets/navigatorsBar/index.styles.ts
@@ -1,6 +1,24 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
-export const NavigatorsBarContainer = styled.div<{ $isNavBar?: boolean }>`
+export const NavigatorsBarContainer = styled.div<{ $isNavBar?: boolean; $shadow?: boolean }>`
+  ${({ theme, $shadow }) =>
+    $shadow &&
+    css`
+      box-shadow: 0px 0px 15px ${theme.colors['yellow-300_80']};
+      animation: shadowPulse 2s ease-in-out infinite;
+
+      @keyframes shadowPulse {
+        0%,
+        100% {
+          box-shadow: 0px 0px 10px ${theme.colors['yellow-300_80']};
+        }
+        50% {
+          box-shadow: 0px 0px 25px ${theme.colors['yellow-300_80']};
+        }
+      }
+    `}
+
   display: flex;
   flex-direction: ${({ $isNavBar }) => ($isNavBar ? 'row' : 'column')};
   align-items: center;
