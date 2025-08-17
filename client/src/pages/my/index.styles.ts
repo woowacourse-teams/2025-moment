7e075f5d8f8be28fdcc31b7b28c44f5dc7eb2166
haku <hakukudh@gmail.com>
2025-08-17 19:22:03 +0900

    feat: 마이페이지 구조 잡기

--- client/src/pages/my/index.styles.ts
+++ client/src/pages/my/index.styles.ts
@@ -7,3 +7,32 @@ export const MyPageWrapper = styled.main`
   justify-content: center;
   align-items: center;
 `;
+
+export const UserInfoSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const StatusSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const SettingSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const Email = styled.p`
+  font-size: 120px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors['gray-400']};
+`;
+
+export const UserInfo = styled.div`
+  display: flex;
+  gap: 16px;
+`;
--- client/src/pages/my/index.tsx
+++ client/src/pages/my/index.tsx
@@ -1,11 +1,18 @@
+import { mockProfile } from '@/features/my/api/mockData';
 import * as S from './index.styles';
 
 export default function MyPage() {
   return (
     <S.MyPageWrapper>
-      <div>userInfo</div>
-      <div>status</div>
-      <div>설정</div>
+      <S.UserInfoSection>
+        <S.Email>{mockProfile.data.email}</S.Email>
+        <S.UserInfo>
+          <p>{mockProfile.data.nickname}</p>
+          <p>{mockProfile.data.level}</p>
+        </S.UserInfo>
+      </S.UserInfoSection>
+      <S.StatusSection>status</S.StatusSection>
+      <S.SettingSection>setting</S.SettingSection>
     </S.MyPageWrapper>
   );
 }
