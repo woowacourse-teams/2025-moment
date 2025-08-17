2b4e68b33ab45dc4d71c29706013b03a9ee10a6e
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-18 00:27:50 +0900

    feat: 새로운 읽지않은 알림이 있을 때 navbar에 있는 모음집이 빛나도록 구현

--- client/src/widgets/navigatorsBar/index.tsx
+++ client/src/widgets/navigatorsBar/index.tsx
@@ -1,9 +1,17 @@
 import { ROUTES } from '@/app/routes/routes';
+import { useNotificationsQuery } from '@/features/notification/hooks/useNotificationsQuery';
+import { sendEvent } from '@/shared/lib/ga';
 import { Link } from 'react-router';
 import * as S from './index.styles';
-import { sendEvent } from '@/shared/lib/ga';
 
 export const NavigatorsBar = ({ $isNavBar }: { $isNavBar?: boolean }) => {
+  const { data: notifications } = useNotificationsQuery();
+
+  if (!notifications) {
+    return null;
+  }
+  const isNotificationExisting = notifications?.data.length > 0;
+
   const handleTodayMomentClick = () => {
     sendEvent({
       category: 'NavigatorsBar',
@@ -29,7 +37,7 @@ export const NavigatorsBar = ({ $isNavBar }: { $isNavBar?: boolean }) => {
   };
 
   return (
-    <S.NavigatorsBarContainer $isNavBar={$isNavBar}>
+    <S.NavigatorsBarContainer $isNavBar={$isNavBar} $shadow={isNotificationExisting}>
       <Link to={ROUTES.TODAY_MOMENT} onClick={handleTodayMomentClick}>
         <S.LinkContainer $isNavBar={$isNavBar}>
           <S.IconImage src="/bluePlanet.png" alt="오늘의 모멘트 페이지로 이동 버튼"></S.IconImage>
