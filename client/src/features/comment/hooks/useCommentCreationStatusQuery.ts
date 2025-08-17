82a3f7f2fe64474ec8d46af8aa25dd373e342f43
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-18 01:42:12 +0900

    chore: 필요없는 파일 제거

--- client/src/features/comment/hooks/useCommentCreationStatusQuery.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { getCommentCreationStatus } from '../api/getCommentCreationStatus';
-
-export const useCommentCreationStatusQuery = () => {
-  return useQuery({
-    queryKey: ['commentCreationStatus'],
-    queryFn: getCommentCreationStatus,
-  });
-};
