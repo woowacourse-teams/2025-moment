480300e5ba3ff8c5b1cb8da1f8e91ccfb79f4857
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-18 16:30:39 +0900

    feaet: ClickMe button 밖을 클릭하면 모달 꺼지도록 useOutsideClick 훅 적용

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,11 +1,12 @@
 import { ROUTES } from '@/app/routes/routes';
+import { useOutsideClick } from '@/shared/hooks';
 import { useDelayedVisible } from '@/shared/hooks/useDelayedVisible';
 import { sendEvent } from '@/shared/lib/ga';
 import { HomePageAnalyticsEvent } from '@/shared/lib/ga/analyticsEvent';
 import { Button } from '@/shared/ui/button/Button';
 import { Hero } from '@/widgets/hero';
 import { NavigatorsBar } from '@/widgets/navigatorsBar';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useNavigate } from 'react-router';
 import * as S from './index.styles';
 
@@ -13,6 +14,17 @@ export default function HomePage() {
   const [isWidgetOpen, setIsWidgetOpen] = useState(false);
   const navigate = useNavigate();
   const { isVisible } = useDelayedVisible({ delay: 100 });
+  const ClickMeRef = useRef<HTMLDivElement | null>(null);
+  const BlackHoleRef = useRef<HTMLDivElement | null>(null);
+
+  useOutsideClick({
+    ref: BlackHoleRef,
+    callback: () => {
+      setIsWidgetOpen(false);
+    },
+    isActive: isWidgetOpen,
+    excludeRefs: [ClickMeRef],
+  });
 
   const handleClick = () => {
     sendEvent(HomePageAnalyticsEvent.ClickTodayMomentButton);
@@ -32,13 +44,13 @@ export default function HomePage() {
       <S.ContentSection isVisible={isVisible}>
         <Button title="모멘트 작성하기" variant="secondary" onClick={handleClick} />
       </S.ContentSection>
-      <S.BlackHoleContainer>
+      <S.BlackHoleContainer ref={ClickMeRef}>
         <S.BlackHoleText>click me</S.BlackHoleText>
         <button onClick={handleWidgetClick}>
-          <S.BlackHoleImage src="/images/blackHole.png" alt="네비게이션 메뉴 열기" />
+          <S.BlackHoleImage src="/images/blackHole.png" alt="네비게이션 `메뉴 열기" />
         </button>
       </S.BlackHoleContainer>
-      <S.ClickMeContainer isWidgetOpen={isWidgetOpen}>
+      <S.ClickMeContainer ref={BlackHoleRef} isWidgetOpen={isWidgetOpen}>
         <NavigatorsBar $isNavBar={false} />
       </S.ClickMeContainer>
     </S.HomePageWrapper>
