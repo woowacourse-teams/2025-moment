8919967252e19524e83e19d170753dca10ba40fb
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-18 13:37:55 +0900

    featL Mobile 일 때 notification 로직 추가

--- client/src/app/layout/ui/Navbar.tsx
+++ client/src/app/layout/ui/Navbar.tsx
@@ -8,10 +8,11 @@ import { Logo } from '@/shared/ui/logo/Logo';
 import { NavigatorsBar } from '@/widgets/navigatorsBar';
 
 import { useCheckIfLoggedInQuery } from '@/features/auth/hooks/useCheckIfLoggedInQuery';
+import { useNotificationsQuery } from '@/features/notification/hooks/useNotificationsQuery';
+import { HomePageAnalyticsEvent } from '@/shared/lib/ga/analyticsEvent';
 import { useRef } from 'react';
 import { Link, useLocation } from 'react-router';
 import * as S from './Navbar.styles';
-import { HomePageAnalyticsEvent } from '@/shared/lib/ga/analyticsEvent';
 
 export type Level = 'METEOR' | 'ASTEROID' | 'COMET';
 
@@ -24,6 +25,10 @@ export const Navbar = () => {
   const { isOpen: isMobileMenuOpen, toggle: toggleMobileMenu } = useToggle(false);
   const mobileMenuRef = useRef<HTMLDivElement>(null);
   const dropdownButtonRef = useRef<HTMLButtonElement>(null);
+  const { data: notifications } = useNotificationsQuery();
+
+  const isNotificationExisting =
+    notifications?.data.length && notifications?.data.length > 0 ? true : false;
 
   if (isError) {
     console.error('checkIfLoggedInQuery error', error);
@@ -68,7 +73,11 @@ export const Navbar = () => {
         <S.MobileMenuContent>
           <S.MobileNavItems>
             {navItems.map(item => (
-              <S.MobileNavItem key={item.href} $isActive={currentPath === item.href}>
+              <S.MobileNavItem
+                key={item.href}
+                $isActive={currentPath === item.href}
+                $shadow={item.label === '나만의 모음집' && isNotificationExisting}
+              >
                 <Link to={item.href} onClick={toggleMobileMenu}>
                   {item.label}
                 </Link>
