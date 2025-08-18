484d6d6352fa46124ecb4d1fecb26e351007401b
haku <hakukudh@gmail.com>
2025-08-18 15:39:47 +0900

    feat: levelTableData 분리

---
+++ client/src/widgets/levelTable/data/levelTableData.ts
@@ -0,0 +1,37 @@
+export const levelTableData = [
+  {
+    id: 1,
+    level: 1,
+    name: '소행성',
+    story: '떠다니던 먼지들이 뭉쳐 자신만의 이야기를 품은 첫 번째 형태',
+    condition: '0p',
+  },
+  {
+    id: 2,
+    level: 2,
+    name: '유성',
+    story: '다른 별의 궤도를 스치며 처음으로 빛의 흔적을 남기는 존재',
+    condition: '20p',
+  },
+  {
+    id: 3,
+    level: 3,
+    name: '혜성',
+    story: '긴 꼬리를 남기며 우주를 가로지르는 방랑자',
+    condition: '200p',
+  },
+  {
+    id: 4,
+    level: 4,
+    name: '행성',
+    story: '자신만의 색과 이야기를 가진 독립적인 존재',
+    condition: '1000p',
+  },
+  {
+    id: 5,
+    level: 5,
+    name: '거대행성',
+    story: '뚜렷한 존재감을 드러내며 주변의 다른 위성들을 끌어당기는 중심',
+    condition: '8000p',
+  },
+];
