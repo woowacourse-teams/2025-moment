fd76dbc20017dff32d1bb345a528c5e1c23aaf99
이상희 <sang6299@naver.com>
2025-08-19 23:20:44 +0900

    style: 모음집 탭 UI 개선

--- client/src/pages/collection/mycomment/index.tsx
+++ client/src/pages/collection/mycomment/index.tsx
@@ -1,5 +1,3 @@
-import { TitleContainer } from '@/shared/ui/titleContainer/TitleContainer';
-
 import { MyCommentsList } from '@/features/comment/ui/MyCommentsList';
 import { CollectionHeader } from '@/pages/collection/CollectionHeader';
 import * as S from '../index.styles';
@@ -8,10 +6,7 @@ export default function MyCommentCollectionPage() {
   return (
     <S.CollectionContainer>
       <CollectionHeader />
-      <TitleContainer
-        title="나의 코멘트"
-        subtitle={'내가 작성한 코멘트와 받은 공감을 확인해보세요'}
-      />
+      <S.Description>내가 작성한 코멘트와 받은 에코를 확인해보세요</S.Description>
       <MyCommentsList />
     </S.CollectionContainer>
   );
