86a03b812368bec3efd1898691a4b968ff75db19
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-19 15:00:13 +0900

    chore: 불필요한 파일 제거

--- client/src/features/moment/hook/useMatchMomentsQuery.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { matchMoments } from '../api/matchMoments';
-
-export const useMatchMomentsQuery = () => {
-  return useQuery({
-    queryKey: ['matchMoments'],
-    queryFn: matchMoments,
-  });
-};
