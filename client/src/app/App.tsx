884475e23cf3d6c051ae0a0198ff5ed3d04e4a7b
eunwoo-levi <eunwoo1341@gmail.com>
2025-08-21 01:31:09 +0900

    feat: ToastProvider를 감싸기 전에 useSSEnotifications가 먼저 실행되도록 구조 변경

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,6 +1,5 @@
 import { queryClient } from '@/app/lib/queryClient';
 import { router } from '@/app/routes';
-import { ToastProvider } from '@/shared/context/toast/ToastProvider';
 import { ErrorBoundary } from '@/shared/ui';
 import { ThemeProvider } from '@emotion/react';
 import { QueryClientProvider } from '@tanstack/react-query';
@@ -14,11 +13,9 @@ const App = () => {
     <ErrorBoundary>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={theme}>
-          <ToastProvider>
-            <GlobalStyles />
-            <RouterProvider router={router} />
-            {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
-          </ToastProvider>
+          <GlobalStyles />
+          <RouterProvider router={router} />
+          {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
         </ThemeProvider>
       </QueryClientProvider>
     </ErrorBoundary>
