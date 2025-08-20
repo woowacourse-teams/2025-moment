7aea38b8d68a1c20f1d00f6f0f37a898886345de
이상희 <sang6299@naver.com>
2025-08-20 21:12:31 +0900

    style: NotFound 컴포넌트 그림 삽입

--- client/src/shared/ui/notFound/NotFound.tsx
+++ client/src/shared/ui/notFound/NotFound.tsx
@@ -27,6 +27,7 @@ export const NotFound = ({
       <S.NotFoundContainer $size={size}>
         <S.NotFoundTitle $size={size}>{title}</S.NotFoundTitle>
         <S.NotFoundSubtitle $size={size}>{subtitle}</S.NotFoundSubtitle>
+        <S.NotFoundImage src={'/images/character.png'} alt="notFound" />
       </S.NotFoundContainer>
     </S.NotFoundWrapper>
   );
