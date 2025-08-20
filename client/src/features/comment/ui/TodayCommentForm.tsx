dfb0df0d216d7df03cc800f5ea21bba0d3c174bc
이상희 <sang6299@naver.com>
2025-08-20 20:52:58 +0900

    style: 작성자 정보 크기 조정

--- client/src/features/comment/ui/TodayCommentForm.tsx
+++ client/src/features/comment/ui/TodayCommentForm.tsx
@@ -7,6 +7,7 @@ import { AlertCircle, Loader, RotateCcw } from 'lucide-react';
 import * as S from './TodayCommentForm.styles';
 import { TodayCommentWriteContent } from './TodayCommentWriteContent';
 import { WriteTime } from '@/shared/ui/writeTime';
+import { WriterInfo } from '@/widgets/writerInfo';
 
 export function TodayCommentForm() {
   const { data: isLoggedIn, isLoading: isLoggedInLoading } = useCheckIfLoggedInQuery();
@@ -27,10 +28,7 @@ export function TodayCommentForm() {
         <Card.TitleContainer
           title={
             <S.TitleWrapper>
-              <S.UserInfoWrapper>
-                <S.NotLoggedIcon src={'/images/firstAsteroid.png'} alt={''} />
-                <S.NotLoggedNickname>푸르른 물방울의 테리우스</S.NotLoggedNickname>
-              </S.UserInfoWrapper>
+              <WriterInfo writer={'푸르른 물방울의 테리우스'} level={'ASTEROID_WHITE'} />
               <S.ActionWrapper>
                 <WriteTime date="9시간 전" />
               </S.ActionWrapper>
@@ -74,13 +72,7 @@ export function TodayCommentForm() {
       <Card.TitleContainer
         title={
           <S.TitleWrapper>
-            <S.UserInfoWrapper>
-              <S.LevelImage
-                src={LEVEL_MAP[momentData.level as keyof typeof LEVEL_MAP]}
-                alt={momentData.level}
-              />
-              <span>{momentData.nickname}</span>
-            </S.UserInfoWrapper>
+            <WriterInfo writer={momentData.nickname} level={momentData.level} />
             <S.ActionWrapper>
               <WriteTime date={momentData.createdAt} />
               <S.RefreshButton onClick={() => refetch()}>
