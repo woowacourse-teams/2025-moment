542040dd10ff6785ab1fb866b4505c43159236b2
yush <yush2202@gmail.com>
2025-09-06 21:25:58 +0900

    refactor: dto 수정
    Co-authored-by: Firedrago95 <yonghwa95@naver.com>

--- server/src/main/java/moment/storage/dto/request/UploadUrlRequest.java
+++ server/src/main/java/moment/storage/dto/request/UploadUrlRequest.java
@@ -1,11 +1,14 @@
 package moment.storage.dto.request;
 
-import jakarta.validation.constraints.NotNull;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 
 public record UploadUrlRequest(
-        @NotNull
+        @Schema(description = "파일 이름", example = "my_cat.jpg")
+        @NotBlank(message = "FILE_NAME_NOT_EXIST")
         String imageName,
-        @NotNull
+        @Schema(description = "파일 타입", example = "jpg")
+        @NotBlank(message = "FILE_TYPE_NOT_EXIST")
         String imageType
 ) {
 }
