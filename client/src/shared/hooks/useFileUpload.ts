bd94a90b7bb4471171ecd55b543c550d8244797f
eunwoo-levi <eunwoo1341@gmail.com>
2025-09-09 22:22:35 +0900

    chore: 역할에 맡게 훅 및 함수 이동

--- client/src/shared/hooks/useFileUpload.ts
+++ client/src/shared/hooks/useFileUpload.ts
@@ -1,5 +1,5 @@
-import { uploadImageToS3 } from '@/features/moment/api/uploadImageToS3';
-import { usePresignedUrlMutation } from '@/features/moment/api/usePresignedUrlQuery';
+import { uploadImageToS3 } from '@/shared/api/uploadImageToS3';
+import { usePresignedUrlMutation } from '@/shared/hooks/usePresignedUrlMutation';
 import { ImageUploadData, UploadedImage } from '@/shared/types';
 import { useRef, useState } from 'react';
 import { useCloudFrontUrl } from './useCloudFrontUrl';
