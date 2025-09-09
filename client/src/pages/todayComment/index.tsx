135d61f5e4c870b3af8e478b64344ad6ebb4999d
이상희 <sang6299@naver.com>
2025-09-09 21:24:24 +0900

    feat: 오늘의 코멘트 태그로 필터하여 요청

--- client/src/pages/todayComment/index.tsx
+++ client/src/pages/todayComment/index.tsx
@@ -1,15 +1,41 @@
 import { TodayCommentForm } from '@/features/comment/ui/TodayCommentForm';
 import { TitleContainer } from '@/shared/ui/titleContainer/TitleContainer';
 import * as S from '../todayMoment/index.styles';
+import { TagList } from '@/features/moment/ui/TagList';
+import { useState } from 'react';
+import { useCommentableMomentsQuery } from '@/features/comment/api/useCommentableMomentsQuery';
+import { useCheckIfLoggedInQuery } from '@/features/auth/api/useCheckIfLoggedInQuery';
 
 export default function TodayCommentPage() {
+  const { data: isLoggedIn, isLoading: isLoggedInLoading } = useCheckIfLoggedInQuery();
+  const [selectedTag, setSelectedTag] = useState<string | undefined>(undefined);
+  const {
+    data: momentData,
+    isLoading,
+    error,
+    refetch,
+  } = useCommentableMomentsQuery({ enabled: isLoggedIn === true }, selectedTag);
+
+  const handleTagClick = (tag: string) => {
+    const newSelectedTag = selectedTag === tag ? undefined : tag;
+    setSelectedTag(newSelectedTag);
+  };
+
   return (
     <S.TodayPageWrapper>
       <TitleContainer
         title="오늘의 코멘트"
         subtitle="특별한 모멘트를 공유받고 따뜻한 공감을 보내보세요"
       />
-      <TodayCommentForm />;
+      <TagList onTagClick={handleTagClick} selectedTag={selectedTag ? [selectedTag] : undefined} />
+      <TodayCommentForm
+        momentData={momentData}
+        isLoading={isLoading}
+        isLoggedIn={isLoggedIn}
+        isLoggedInLoading={isLoggedInLoading}
+        error={error}
+        refetch={refetch}
+      />
     </S.TodayPageWrapper>
   );
 }
