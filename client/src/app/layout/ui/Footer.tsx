b61194f72de9f7558fc8f51d3911536f661b0260
이상희 <sang6299@naver.com>
2025-09-17 17:38:46 +0900

    refactor: 카카오 오픈채팅 링크 변경 및 css 분리

--- client/src/app/layout/ui/Footer.tsx
+++ client/src/app/layout/ui/Footer.tsx
@@ -1,34 +1,15 @@
-import styled from '@emotion/styled';
+import * as S from './Footer.styles';
 
 export const Footer = () => {
   return (
-    <FooterWrapper>
+    <S.FooterWrapper>
       <div>
         문의: &nbsp;
-        <InquiryLink href="https://open.kakao.com/o/shuZJySh" target="_blank">
+        <S.InquiryLink href="http://pf.kakao.com/_txihUn" target="_blank">
           카카오톡 오픈 채팅
-        </InquiryLink>
+        </S.InquiryLink>
       </div>
       <div>Copyright © 우아한테크코스 모멘트 팀 All Rights Reserved.</div>
-    </FooterWrapper>
+    </S.FooterWrapper>
   );
 };
-
-const FooterWrapper = styled.div`
-  width: 100%;
-  height: 100px;
-  color: ${({ theme }) => theme.colors['gray-600']};
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 4px;
-`;
-
-const InquiryLink = styled.a`
-  color: ${({ theme }) => theme.colors['gray-600']};
-  text-decoration: underline;
-  &:hover {
-    color: ${({ theme }) => theme.colors['yellow-300_80']};
-  }
-`;
