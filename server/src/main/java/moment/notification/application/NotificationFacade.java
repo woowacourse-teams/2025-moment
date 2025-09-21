564b269822729ff519b71cfae558f2e76136b23a
yush <yush2202@gmail.com>
2025-09-22 00:50:27 +0900

    refactor: 알림 전송 기능 리팩터링

---
+++ server/src/main/java/moment/notification/application/NotificationFacade.java
@@ -0,0 +1,39 @@
+package moment.notification.application;
+
+import lombok.RequiredArgsConstructor;
+import moment.global.domain.TargetType;
+import moment.notification.domain.Notification;
+import moment.notification.domain.NotificationType;
+import moment.notification.dto.response.NotificationSseResponse;
+import moment.user.domain.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class NotificationFacade {
+
+    private final NotificationService notificationService;
+    private final SseNotificationService sseNotificationService;
+
+    @Transactional
+    public void sendSseNotificationAndNotification(User momenter,
+                                                   Long momentId,
+                                                   NotificationType notificationType,
+                                                   TargetType targetType) {
+        Notification savedNotification = notificationService.saveNotification(
+                momenter,
+                momentId,
+                notificationType,
+                targetType);
+
+        NotificationSseResponse response = NotificationSseResponse.createSseResponse(
+                savedNotification.getId(),
+                NotificationType.NEW_COMMENT_ON_MOMENT,
+                TargetType.MOMENT,
+                momentId);
+
+        sseNotificationService.sendToClient(momenter.getId(), "notification", response);
+    }
+}
