7c5305a4179d01e656aaf271210694aa5f939e88
Firedrago95 <yonghwa95@naver.com>
2025-09-28 17:47:54 +0900

    fix: 로깅 전략 적용

--- server/src/main/java/moment/global/logging/RepositoryLogAspect.java
+++ server/src/main/java/moment/global/logging/RepositoryLogAspect.java
@@ -1,18 +1,20 @@
 package moment.global.logging;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
 @Aspect
 @Component
 @Slf4j
-@Profile({"test", "dev"})
 public class RepositoryLogAspect {
+
+    private static final long SLOW_QUERY_THRESHOLD_MS = 500;
 
     @Around("execution(public * org.springframework.data.repository.Repository+.*(..))")
     public Object logRepository(ProceedingJoinPoint joinPoint) throws Throwable {
@@ -23,8 +25,20 @@ public class RepositoryLogAspect {
 
         stopWatch.stop();
         long totalTimeMillis = stopWatch.getTotalTimeMillis();
+        String methodName = joinPoint.getSignature().getName();
 
-        log.info("query: [{}] totalTime: [{}] ms", joinPoint.getSignature().getName(), totalTimeMillis);
+        if (totalTimeMillis > SLOW_QUERY_THRESHOLD_MS) {
+            log.warn("Slow Repository",
+                    kv("method", methodName),
+                    kv("duration_ms", totalTimeMillis)
+            );
+            return result;
+        }
+
+        log.debug("Repository Executed",
+                kv("method", methodName),
+                kv("duration_ms", totalTimeMillis)
+        );
 
         return result;
     }
