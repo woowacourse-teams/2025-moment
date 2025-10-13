43c7ddfedce4d2f031514f21152160664fbcee8a
mimi <110226866+hongkikii@users.noreply.github.com>
2025-10-13 16:20:14 +0900

    test: NotificationRepository 테스트 코드 리팩토링

--- server/src/test/java/moment/notification/infrastructure/NotificationRepositoryTest.java
+++ server/src/test/java/moment/notification/infrastructure/NotificationRepositoryTest.java
@@ -6,7 +6,6 @@ import moment.global.domain.TargetType;
 import moment.user.domain.ProviderType;
 import moment.user.domain.User;
 import moment.user.infrastructure.UserRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
@@ -18,6 +17,7 @@ import org.springframework.test.context.ActiveProfiles;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("test")
 @DataJpaTest
@@ -31,23 +31,25 @@ class NotificationRepositoryTest {
     private UserRepository userRepository;
 
     private User user;
-    private User notTargetUser;
+    private User anotherUser;
 
-    private final boolean isReadFlag = true;
-    private final boolean isNotReadFlag = false;
+    private final boolean readFlag = true;
+    private final boolean unReadFlag = false;
     private final long contentId = 1L;
+    private long userId;
 
     @BeforeEach
     void setUp() {
         user = new User("lebron@james.com", "lebron1234!", "르브론", ProviderType.EMAIL);
         userRepository.save(user);
+        userId = user.getId();
 
-        notTargetUser = new User("mimi@icloud.com", "mimi1234!", "밍밍", ProviderType.EMAIL);
-        userRepository.save(notTargetUser);
+        anotherUser = new User("mimi@icloud.com", "mimi1234!", "밍밍", ProviderType.EMAIL);
+        userRepository.save(anotherUser);
     }
 
     @Test
-    void 읽지_않은_알림을_조회한다() {
+    void user_id를_고려하여_읽지_않은_알림들을_조회한다() {
         // given
         TargetType contentType = TargetType.MOMENT;
         NotificationType reason = NotificationType.NEW_COMMENT_ON_MOMENT;
@@ -55,25 +57,27 @@ class NotificationRepositoryTest {
         Notification unreadNotification1 = new Notification(user, reason, contentType, contentId);
         Notification unreadNotification2 = new Notification(user, reason, contentType, contentId);
         Notification unreadNotification3 = new Notification(user, reason, contentType, contentId);
-        Notification notTargetUserNotification = new Notification(notTargetUser, reason, contentType, contentId);
+        Notification anotherUserNotification = new Notification(anotherUser, reason, contentType, contentId);
 
         notificationRepository.save(unreadNotification1);
         notificationRepository.save(unreadNotification2);
         notificationRepository.save(unreadNotification3);
-        notificationRepository.save(notTargetUserNotification);
+        notificationRepository.save(anotherUserNotification);
 
         // when
-        List<Notification> unreadNotifications = notificationRepository.findAllByUserAndIsRead(user, isNotReadFlag);
+        List<Notification> unreadNotifications = notificationRepository.findAllByUserIdAndIsRead(userId, unReadFlag);
 
         // then
-        assertThat(unreadNotifications).hasSize(3);
-        assertThat(unreadNotifications.stream()
-                .noneMatch(Notification::isRead))
-                .isTrue();
+        assertAll(
+                () -> assertThat(unreadNotifications).hasSize(3),
+                () -> assertThat(unreadNotifications.stream()
+                        .noneMatch(Notification::isRead))
+                        .isTrue()
+        );
     }
 
     @Test
-    void 읽은_알림을_조회한다() {
+    void user_id를_고려하여_읽은_알림들을_조회한다() {
         // given
         TargetType contentType = TargetType.MOMENT;
         NotificationType reason = NotificationType.NEW_COMMENT_ON_MOMENT;
@@ -81,59 +85,95 @@ class NotificationRepositoryTest {
         Notification unreadNotification1 = new Notification(user, reason, contentType, contentId);
         Notification readNotification1 = new Notification(user, reason, contentType, contentId);
         Notification readNotification2 = new Notification(user, reason, contentType, contentId);
-        Notification notTargetUserNotification = new Notification(notTargetUser, reason, contentType, contentId);
+        Notification anotherUserNotification = new Notification(anotherUser, reason, contentType, contentId);
 
         readNotification1.checkNotification();
         readNotification2.checkNotification();
-        notTargetUserNotification.checkNotification();
+        anotherUserNotification.checkNotification();
 
         notificationRepository.save(unreadNotification1);
         notificationRepository.save(readNotification1);
         notificationRepository.save(readNotification2);
-        notificationRepository.save(notTargetUserNotification);
+        notificationRepository.save(anotherUserNotification);
 
         // when
-        List<Notification> readNotifications = notificationRepository.findAllByUserAndIsRead(user, isReadFlag);
+        List<Notification> readNotifications = notificationRepository.findAllByUserIdAndIsRead(userId, readFlag);
 
         // then
-        assertThat(readNotifications).hasSize(2);
-        assertThat(readNotifications.stream()
-                .allMatch(Notification::isRead))
-                .isTrue();
+        assertAll(
+                () -> assertThat(readNotifications).hasSize(2),
+                () -> assertThat(readNotifications.stream()
+                        .allMatch(Notification::isRead))
+                        .isTrue()
+        );
     }
 
     @Test
-    void 읽지_않은_알림을_타겟_타입과_함께_조회한다() {
+    void user_id와_읽음_여부와_타겟_타입을_고려하여_알림의_컨텐츠_id들을_조회한다() {
         // given
-        TargetType targetContentType = TargetType.MOMENT;
-        TargetType notTargetContentType = TargetType.COMMENT;
-        NotificationType targetReason = NotificationType.NEW_COMMENT_ON_MOMENT;
-        NotificationType notTargetReason = NotificationType.NEW_REPLY_ON_COMMENT;
+        TargetType contentType = TargetType.MOMENT;
+        TargetType anotherContentType = TargetType.COMMENT;
+        NotificationType reason = NotificationType.NEW_COMMENT_ON_MOMENT;
+        NotificationType anotherReason = NotificationType.NEW_REPLY_ON_COMMENT;
 
-        Notification expectedNotification = new Notification(user, targetReason, targetContentType, contentId);
-        Notification readNotification = new Notification(user, targetReason, targetContentType, contentId);
-        Notification notContentTypeNotification = new Notification(user, notTargetReason, notTargetContentType, contentId);
-        Notification notTargetUserNotification = new Notification(notTargetUser, targetReason, targetContentType, contentId);
+        Notification expectedNotification = new Notification(user, reason, contentType, contentId);
+        Notification readNotification = new Notification(user, reason, contentType, contentId);
+        Notification anotherTypeNotification = new Notification(user, anotherReason, anotherContentType, contentId);
+        Notification anotherUserNotification = new Notification(anotherUser, reason, contentType, contentId);
 
         readNotification.checkNotification();
 
-        notificationRepository.save(expectedNotification);
+        Notification savedExpectedNotification = notificationRepository.save(expectedNotification);
         notificationRepository.save(readNotification);
-        notificationRepository.save(notContentTypeNotification);
-        notificationRepository.save(notTargetUserNotification);
+        notificationRepository.save(anotherTypeNotification);
+        notificationRepository.save(anotherUserNotification);
 
         // when
-
-        List<Notification> expectedNotifications = notificationRepository.findAllByUserAndIsReadAndTargetType(
-                user, isNotReadFlag, targetContentType);
+        List<Long> expectedContentIds = notificationRepository.findAllByUserIdAndIsReadAndTargetType(
+                userId, unReadFlag, contentType);
 
         // then
-        Assertions.assertAll(
-                () -> assertThat(expectedNotifications).hasSize(1),
-                () -> assertThat(expectedNotifications.stream()
-                        .allMatch(notification -> !notification.isRead()
-                                && notification.getTargetType() == targetContentType))
-                        .isTrue()
+        assertThat(expectedContentIds).hasSize(1);
+    }
+
+    @Test
+    void 읽음_여부와_타겟_타입으로_알림_목록을_조회한다() {
+        // given
+        Long contentId1 = 1L;
+        Long contentId2 = 2L;
+        TargetType contentType = TargetType.MOMENT;
+        TargetType anotherContentType = TargetType.COMMENT;
+        NotificationType reason = NotificationType.NEW_COMMENT_ON_MOMENT;
+
+        Notification expectedNotification1 = new Notification(user, reason, contentType, contentId1);
+        Notification expectedNotification2 = new Notification(user, reason, contentType, contentId2);
+
+        Notification readNotification = new Notification(user, reason, contentType, contentId1);
+        readNotification.checkNotification();
+
+        Notification anotherTypeNotification = new Notification(user, reason, anotherContentType, contentId1);
+        Notification anotherUserNotification = new Notification(anotherUser, reason, contentType, contentId1);
+
+        notificationRepository.saveAll(
+                List.of(expectedNotification1,
+                        expectedNotification2,
+                        anotherUserNotification,
+                        readNotification,
+                        anotherTypeNotification));
+
+        List<Long> contentIds = List.of(contentId1, contentId2);
+
+        // when
+        List<Notification> foundNotifications = notificationRepository.findNotificationsBy(
+                contentIds, unReadFlag, contentType);
+
+        // then
+        assertAll(
+                () -> assertThat(foundNotifications).hasSize(3),
+                () -> assertThat(foundNotifications).containsExactlyInAnyOrder(
+                        expectedNotification1,
+                        expectedNotification2,
+                        anotherUserNotification)
         );
     }
 }
