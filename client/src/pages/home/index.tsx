14afc58a803cebc5ce2ce6b54f9558a76228985d
이상희 <sang6299@naver.com>
2025-10-15 16:57:13 +0900

    refactor: blackhole 관련 코드 제거

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,42 +1,19 @@
 import { ROUTES } from '@/app/routes/routes';
-import { useOutsideClick } from '@/shared/hooks';
 import { useDelayedVisible } from '@/shared/hooks/useDelayedVisible';
 import { sendEvent } from '@/shared/lib/ga';
 import { HomePageAnalyticsEvent } from '@/shared/lib/ga/analyticsEvent';
 import { Button } from '@/shared/ui/button/Button';
-import { LazyImage } from '@/shared/ui/lazyImage/LazyImage';
 import { Hero } from '@/widgets/hero';
-import { NavigatorsBar } from '@/widgets/navigatorsBar';
-import { useRef, useState } from 'react';
 import { useNavigate } from 'react-router';
 import * as S from './index.styles';
-import { useCheckIfLoggedInQuery } from '@/features/auth/api/useCheckIfLoggedInQuery';
 
 export default function HomePage() {
-  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
   const navigate = useNavigate();
   const { isVisible } = useDelayedVisible({ delay: 100 });
-  const { data: isLoggedIn } = useCheckIfLoggedInQuery();
-  const ClickMeRef = useRef<HTMLDivElement | null>(null);
-  const BlackHoleRef = useRef<HTMLDivElement | null>(null);
-
-  useOutsideClick({
-    ref: BlackHoleRef,
-    callback: () => {
-      setIsWidgetOpen(false);
-    },
-    isActive: isWidgetOpen,
-    excludeRefs: [ClickMeRef],
-  });
 
   const handleClick = () => {
     sendEvent(HomePageAnalyticsEvent.ClickTodayMomentButton);
     navigate(ROUTES.TODAY_MOMENT);
-  };
-
-  const handleWidgetClick = () => {
-    sendEvent(HomePageAnalyticsEvent.ClickBlackHoleButton);
-    setIsWidgetOpen(!isWidgetOpen);
   };
 
   return (
@@ -63,26 +40,6 @@ export default function HomePage() {
           <S.IntroText>나의 소소한 이야기가 누군가에게 공감으로 닿는 순간.</S.IntroText>
         </S.IntroTextWrapper>
       </S.IntroSection>
-      {isLoggedIn && (
-        <>
-          <S.BlackHoleContainer ref={ClickMeRef}>
-            <S.BlackHoleText>click me</S.BlackHoleText>
-            <button onClick={handleWidgetClick}>
-              <LazyImage
-                src="/images/blackHole.webp"
-                alt="네비게이션 `메뉴 열기"
-                variant="blackHole"
-                width="150px"
-                height="150px"
-                borderRadius="50%"
-              />
-            </button>
-          </S.BlackHoleContainer>
-          <S.ClickMeContainer ref={BlackHoleRef} isWidgetOpen={isWidgetOpen}>
-            <NavigatorsBar $isNavBar={false} />
-          </S.ClickMeContainer>
-        </>
-      )}
     </S.HomePageWrapper>
   );
 }
