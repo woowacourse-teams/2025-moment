a5377969b49b5863990e51ef90c329fbe2137224
이상희 <sang6299@naver.com>
2025-10-17 14:44:44 +0900

    refactor: ExplainSection 컴포넌트 재사용

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -16,6 +16,9 @@ export default function HomePage() {
     intro1: false,
     intro2: false,
     intro3: false,
+    intro4: false,
+    intro5: false,
+    intro6: false,
   });
 
   const intro1Ref = useScrollAnimation({
@@ -31,11 +34,15 @@ export default function HomePage() {
   });
 
   const intro4Ref = useScrollAnimation({
-    onVisible: () => setVisibleItems(prev => ({ ...prev, intro3: true })),
+    onVisible: () => setVisibleItems(prev => ({ ...prev, intro4: true })),
   });
 
   const intro5Ref = useScrollAnimation({
-    onVisible: () => setVisibleItems(prev => ({ ...prev, intro3: true })),
+    onVisible: () => setVisibleItems(prev => ({ ...prev, intro5: true })),
+  });
+
+  const intro6Ref = useScrollAnimation({
+    onVisible: () => setVisibleItems(prev => ({ ...prev, intro6: true })),
   });
 
   const handleClick = () => {
@@ -68,59 +75,68 @@ export default function HomePage() {
         </S.IntroSection>
       </S.IntroSectionWrapper>
       <S.IntroSectionWrapper ref={intro2Ref} isVisible={visibleItems.intro2}>
-        <S.ExplainSection>
-          <S.ExplainImage src="/images/intro1.png" alt="" />
-          <S.ExplainContainer>
-            <S.ExplainTitle>오늘의 모멘트</S.ExplainTitle>
-            <S.ExplainText>하루에 한번, 당신의 특별한 모멘트를 공유해보세요.</S.ExplainText>
-            <S.ExplainText>작성한 모멘트가 익명으로 다른 사용자에게 공유됩니다.</S.ExplainText>
-          </S.ExplainContainer>
-        </S.ExplainSection>
-      </S.IntroSectionWrapper>
-      <S.IntroSectionWrapper ref={intro2Ref} isVisible={visibleItems.intro3}>
-        <S.ExplainSection>
-          <S.ExplainImage src="/images/intro2.png" alt="" />
-          <S.ExplainContainer>
-            <S.ExplainTitle>오늘의 코멘트</S.ExplainTitle>
-            <S.ExplainText>특별한 모멘트를 공유받고 따뜻한 공감을 보내보세요.</S.ExplainText>
-            <S.ExplainText>다른 사람의 모멘트에 코멘트를 작성할 수 있어요.</S.ExplainText>
-          </S.ExplainContainer>
-        </S.ExplainSection>
+        <ExplainSection
+          image="/images/intro1.png"
+          title="오늘의 모멘트"
+          text1="하루에 한번, 당신의 특별한 모멘트를 공유해보세요."
+          text2="작성한 모멘트가 익명으로 다른 사용자에게 공유됩니다."
+        />
       </S.IntroSectionWrapper>
       <S.IntroSectionWrapper ref={intro3Ref} isVisible={visibleItems.intro3}>
-        <S.ExplainSection>
-          <S.ExplainImage src="/images/intro3.png" alt="" />
-          <S.ExplainContainer>
-            <S.ExplainTitle>나의 모멘트 모음집</S.ExplainTitle>
-            <S.ExplainText>
-              내가 작성했던 모멘트와, 다른 사용자들이 달아준 코멘트를 확인할 수 있어요.
-            </S.ExplainText>
-            <S.ExplainText>받은 코멘트에 에코를 보내 따뜻한 마음을 전달해보세요.</S.ExplainText>
-          </S.ExplainContainer>
-        </S.ExplainSection>
+        <ExplainSection
+          image="/images/intro2.png"
+          title="오늘의 코멘트"
+          text1="특별한 모멘트를 공유받고 따뜻한 공감을 보내보세요."
+          text2="다른 사람의 모멘트에 코멘트를 작성할 수 있어요."
+        />
       </S.IntroSectionWrapper>
-      <S.IntroSectionWrapper ref={intro4Ref} isVisible={visibleItems.intro3}>
-        <S.ExplainSection>
-          <S.ExplainImage src="/images/intro4.png" alt="" />
-          <S.ExplainContainer>
-            <S.ExplainTitle>나의 코멘트 모음집</S.ExplainTitle>
-            <S.ExplainText>내가 전달한 코멘트와 해당 모멘트를 확인할 수 있어요.</S.ExplainText>
-            <S.ExplainText>받은 에코도 확인할 수 있어요.</S.ExplainText>
-          </S.ExplainContainer>
-        </S.ExplainSection>
+      <S.IntroSectionWrapper ref={intro4Ref} isVisible={visibleItems.intro4}>
+        <ExplainSection
+          image="/images/intro3.png"
+          title="나의 모멘트 모음집"
+          text1="내가 작성했던 모멘트와, 다른 사용자들이 달아준 코멘트를 확인할 수 있어요."
+          text2="받은 코멘트에 에코를 보내 따뜻한 마음을 전달해보세요."
+        />
       </S.IntroSectionWrapper>
-      <S.IntroSectionWrapper ref={intro5Ref} isVisible={visibleItems.intro3}>
-        <S.ExplainSection>
-          <S.ExplainImage src="/images/intro5.png" alt="" />
-          <S.ExplainContainer>
-            <S.ExplainTitle>레벨업과 별조각을 모으는 재미</S.ExplainTitle>
-            <S.ExplainText>
-              모멘트를 작성하고 코멘트를 달며 레벨을 올리고 별조각을 모을 수 있어요.
-            </S.ExplainText>
-            <S.ExplainText>별조각을 모아 레벨업과 다양한 기능을 사용할 수 있어요.</S.ExplainText>
-          </S.ExplainContainer>
-        </S.ExplainSection>
+      <S.IntroSectionWrapper ref={intro5Ref} isVisible={visibleItems.intro5}>
+        <ExplainSection
+          image="/images/intro4.png"
+          title="나의 코멘트 모음집"
+          text1="내가 전달한 코멘트와 해당 모멘트를 확인할 수 있어요."
+          text2="받은 에코도 확인할 수 있어요."
+        />
+      </S.IntroSectionWrapper>
+      <S.IntroSectionWrapper ref={intro6Ref} isVisible={visibleItems.intro6}>
+        <ExplainSection
+          image="/images/intro5.png"
+          title="레벨업과 별조각을 모으는 재미"
+          text1="모멘트를 작성하고 코멘트를 달며 레벨을 올리고 별조각을 모을 수 있어요."
+          text2="별조각을 모아 레벨업과 다양한 기능을 사용할 수 있어요."
+        />
       </S.IntroSectionWrapper>
     </S.HomePageWrapper>
   );
 }
+
+const ExplainSection = ({
+  image,
+  title,
+  text1,
+  text2,
+}: {
+  image: string;
+  title: string;
+  text1?: string;
+  text2?: string;
+}) => {
+  return (
+    <S.ExplainSection>
+      <S.ExplainImage src={image} alt="" />
+      <S.ExplainContainer>
+        <S.ExplainTitle>{title}</S.ExplainTitle>
+        <S.ExplainText>{text1}</S.ExplainText>
+        <S.ExplainText>{text2}</S.ExplainText>
+      </S.ExplainContainer>
+    </S.ExplainSection>
+  );
+};
