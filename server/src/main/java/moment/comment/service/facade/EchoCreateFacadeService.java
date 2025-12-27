e2c0f5133a3e1ad4409288d394b765aab57f2f4e
yush <yush2202@gmail.com>
2025-12-27 22:17:19 +0900

    chore: 주석 제거

--- server/src/main/java/moment/comment/service/facade/EchoCreateFacadeService.java
+++ server/src/main/java/moment/comment/service/facade/EchoCreateFacadeService.java
@@ -7,7 +7,6 @@ import moment.comment.dto.EchoCreateEvent;
 import moment.comment.dto.request.EchoCreateRequest;
 import moment.comment.service.application.CommentApplicationService;
 import moment.moment.service.application.MomentApplicationService;
-import moment.notification.service.application.NotificationApplicationService;
 import moment.reward.domain.Reason;
 import moment.reward.service.application.RewardApplicationService;
 import org.springframework.context.ApplicationEventPublisher;
@@ -21,7 +20,6 @@ public class EchoCreateFacadeService {
 
     private final CommentApplicationService commentApplicationService;
     private final MomentApplicationService momentApplicationService;
-    private final NotificationApplicationService notificationApplicationService;
     private final RewardApplicationService rewardApplicationService;
 
     private final ApplicationEventPublisher publisher;
@@ -34,16 +32,6 @@ public class EchoCreateFacadeService {
         commentApplicationService.createEcho(request.commentId(), momenterId, request.echoTypes());
         rewardApplicationService.rewardForEcho(commentForEcho.commenterId(), Reason.ECHO_RECEIVED, request.commentId());
 
-        // 에코가 코멘트에 달림 -> 코멘트를 작성한 사람에게 알림 전송 & 저장
-        // 해당 코멘트에 대한 알림 -> 코멘트에 알림을 저장해야 함
         publisher.publishEvent(new EchoCreateEvent(request.commentId(), commentForEcho.commenterId()));
-
-//        notificationApplicationService.createNotificationAndSendSse(
-//                commentForEcho.commenterId(),
-//                request.commentId(),
-//                NotificationType.NEW_REPLY_ON_COMMENT,
-//                TargetType.COMMENT
-//        );
-
     }
 }
