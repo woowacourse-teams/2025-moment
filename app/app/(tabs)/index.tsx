5c70359f3bcbd6318c522a32e3ec5ab9e71e61e7
eunwoo-levi <eunwoo1341@gmail.com>
2026-01-10 07:34:20 +0900

    feat: webview에서 oauth 지원을 위해 userAgent 추가

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -27,6 +27,7 @@ export default function HomeScreen() {
           domStorageEnabled
           sharedCookiesEnabled
           thirdPartyCookiesEnabled
+          userAgent="Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1"
           onLoadStart={handlers.onLoadStart}
           onLoadEnd={handlers.onLoadEnd}
           onNavigationStateChange={handlers.onNavigationStateChange}
