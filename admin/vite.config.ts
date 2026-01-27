4a04ecea986be276602aa8182b25a5688fab86f1
eunwoo-levi <eunwoo1341@gmail.com>
2026-01-27 21:26:39 +0900

    build: vite config 수정

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,22 +1,27 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig, type UserConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import type { InlineConfig } from "vitest";
+
+interface VitestConfigExport extends UserConfig {
+  test?: InlineConfig;
+}
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@features': path.resolve(__dirname, './src/features'),
-      '@shared': path.resolve(__dirname, './src/shared'),
+      "@": path.resolve(__dirname, "./src"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
     },
   },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
     css: true,
   },
-});
+} as VitestConfigExport);
