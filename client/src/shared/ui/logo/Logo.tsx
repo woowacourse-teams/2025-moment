762d095d4e3a60a5bbf4ce61479ca4d4d4fffbff
eunwoo-levi <eunwoo1341@gmail.com>
2026-01-27 17:08:26 +0900

    refactor: 웹에서는 로고 클릭 시 홈으로 이동

--- client/src/shared/ui/logo/Logo.tsx
+++ client/src/shared/ui/logo/Logo.tsx
@@ -1,10 +1,18 @@
+import { Link } from 'react-router';
+import { isApp } from '@/shared/utils/device';
 import * as S from './Logo.styles';
 
 export const Logo = () => {
-  return (
-    <S.LogoButton>
+  const content = (
+    <S.LogoButton as={isApp() ? 'button' : undefined}>
       <S.LogoImage webpSrc="/images/logo.webp" fallbackSrc="/images/fallback/logo.png" alt="" />
       <S.LogoText>Moment</S.LogoText>
     </S.LogoButton>
   );
+
+  if (isApp()) {
+    return content;
+  }
+
+  return <Link to="/">{content}</Link>;
 };
