0ea3c571e0d2262a8f7e591740c417c5bd6e4e9d
eunwoo-levi <eunwoo1341@gmail.com>
2026-02-23 18:35:02 +0900

    feat: app에 iosClientId추가

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -8,7 +8,7 @@ import { GroupProvider } from "@/context/GroupContext";
 export default function RootLayout() {
   useEffect(() => {
     GoogleSignin.configure({
-      iosClientId: "PLACEHOLDER_CLIENT_ID_FOR_DEV",
+      iosClientId: "168098995955-ar8an6g4vp44u4lbsqasb5puahdlapil.apps.googleusercontent.com",
       webClientId:
         "168098995955-rhe4o1m3gnlnab3cpcd4qqv7i3ums1vh.apps.googleusercontent.com",
     });
