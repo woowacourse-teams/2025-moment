09258ae5b63313bde117f683d07e5933a02150ea
yush <yush2202@gmail.com>
2026-03-18 17:07:39 +0900

    fix: api 경로 수정

--- server/src/main/java/moment/question/presentation/QuestionController.java
+++ server/src/main/java/moment/question/presentation/QuestionController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v2/questions")
+@RequestMapping("/api/v2/questions")
 @RequiredArgsConstructor
 public class QuestionController {
 
