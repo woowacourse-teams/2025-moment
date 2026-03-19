2b758458a86e424719d1668b59da76717e38b47c
yush <yush2202@gmail.com>
2026-03-19 21:12:51 +0900

    fix: @EnableJpaAuditing 중복선언 수정

--- server/api/src/main/java/moment/ApiApplication.java
+++ server/api/src/main/java/moment/ApiApplication.java
@@ -4,12 +4,10 @@ import moment.question.infrastructure.properties.GeminiProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableJpaAuditing
 @EnableScheduling
 @EnableAsync
 @EnableRetry
