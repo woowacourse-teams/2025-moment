c5480aa078616ce70dbfc7518fc3e1576d45b614
eunwoo-levi <eunwoo1341@gmail.com>
2026-03-30 16:35:23 +0900

    refactor: CardTitleContainer에 forwardRef 추가 및 Icon color semantic 토큰 적용

--- client/src/shared/design-system/card/CardTitleContainer.tsx
+++ client/src/shared/design-system/card/CardTitleContainer.tsx
@@ -1,6 +1,7 @@
 import { LucideIcon } from 'lucide-react';
 import * as S from './CardTitleContainer.styles';
 import { useTheme } from '@emotion/react';
+import React from 'react';
 
 interface CardTitleContainerProps {
   Icon?: LucideIcon;
@@ -8,15 +9,19 @@ interface CardTitleContainerProps {
   subtitle: React.ReactNode;
 }
 
-export const CardTitleContainer = ({ Icon, title, subtitle }: CardTitleContainerProps) => {
-  const theme = useTheme();
-  return (
-    <S.CardTitleContainer>
-      <S.CardTitleWrapper>
-        {Icon && <Icon size={32} color={theme.colors['yellow-500']} />}
-        <S.CardTitle>{title}</S.CardTitle>
-      </S.CardTitleWrapper>
-      <S.CardSubtitle>{subtitle}</S.CardSubtitle>
-    </S.CardTitleContainer>
-  );
-};
+export const CardTitleContainer = React.forwardRef<HTMLDivElement, CardTitleContainerProps>(
+  ({ Icon, title, subtitle }, ref) => {
+    const theme = useTheme();
+    return (
+      <S.CardTitleContainer ref={ref}>
+        <S.CardTitleWrapper>
+          {Icon && <Icon size={32} color={theme.semantic.color.brand.primary} />}
+          <S.CardTitle>{title}</S.CardTitle>
+        </S.CardTitleWrapper>
+        <S.CardSubtitle>{subtitle}</S.CardSubtitle>
+      </S.CardTitleContainer>
+    );
+  },
+);
+
+CardTitleContainer.displayName = 'CardTitleContainer';
