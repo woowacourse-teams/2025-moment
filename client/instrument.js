193694a5be0631c4fd1dbb69d346b65d65c51099
이상희 <sang6299@naver.com>
2026-04-20 14:45:36 +0900

    refactor: sentry 초기화 코드 리팩토링

--- client/instrument.js
+++ client/instrument.js
@@ -1,21 +1,13 @@
 import * as Sentry from '@sentry/react';
 
-const ERROR_SAMPLE_RATE = 1.0;
-
-function getEnvironmentFromDomain() {
-  if (typeof window === 'undefined') return 'development'; // SSR 대응
-  return window.location.hostname === 'connectingmoment.com' ? 'production' : 'development';
-}
-
 Sentry.init({
   dsn: process.env.REACT_APP_SENTRY_DSN,
-  enabled: getEnvironmentFromDomain() === 'production',
-  environment: getEnvironmentFromDomain(),
-
+  environment: process.env.NODE_ENV,
+  tracesSampleRate: 1.0,
   integrations: [
     Sentry.replayIntegration({
       sessionSampleRate: 0,
-      errorSampleRate: ERROR_SAMPLE_RATE,
+      errorSampleRate: 1.0,
       maskAllText: true,
       maskAllInputs: true,
     }),
